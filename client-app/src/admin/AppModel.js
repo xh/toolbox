f4850741230405691ec47fee226ac196581334b4
Anselm McClain <atm@xh.io>
2018-05-22 16:44:32 -0700

    Hoist Admin console AppModel implements checkAccess()
    + We have a strong standard that admins must have the HOIST_ADMIN role - it's baked into the server-side controllers - so it seems reasonable / prudent to implement this check in the superclass.

--- client-app/src/admin/AppModel.js
+++ client-app/src/admin/AppModel.js
@@ -1,14 +1,6 @@
 import {AppModel as HoistAdminAppModel} from '@xh/hoist/admin';
-import {XH} from '@xh/hoist/core';
 
 export class AppModel extends HoistAdminAppModel {
-
-    checkAccess() {
-        const role = 'HOIST_ADMIN',
-            hasAccess = XH.getUser().hasRole(role),
-            message = hasAccess ? '' : `Role "${role}" is required to use this application.`;
-        return {hasAccess, message};
-    }
 
     get enableLogout() {return true}
 
