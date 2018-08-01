4edbb0bfbfa06f510eedb10bee17310ec35e5adf
Tom Tirapani <hello@tomtirapani.com>
2018-08-01 19:06:17 +0100

    Mobile toolbox: + Added homepage shortcuts to different toolbox pages

--- client-app/src/mobile/App.js
+++ client-app/src/mobile/App.js
@@ -7,14 +7,10 @@
 import {XH, HoistApp} from '@xh/hoist/core';
 import {NavigatorModel} from '@xh/hoist/mobile/cmp/navigator';
 import {AppMenuModel} from '@xh/hoist/mobile/cmp/header';
-import {Icon} from '@xh/hoist/icon';
 
 import {AppContainer} from '@xh/hoist/mobile/appcontainer';
 import {AppComponent} from './AppComponent';
-
 import {homePage} from './home/HomePage';
-import {gridPage} from './grids/GridPage';
-import {iconPage} from './icons/IconPage';
 
 @HoistApp
 class AppClass {
@@ -36,20 +32,7 @@ class AppClass {
     }
 
     constructor() {
-        this.appMenuModel = new AppMenuModel({
-            itemModels: [
-                {
-                    icon: Icon.grid({prefix: 'fal'}),
-                    text: 'Grids',
-                    action: () => this.navigate('Grids', gridPage)
-                },
-                {
-                    icon: Icon.edit(),
-                    text: 'Icons',
-                    action: () => this.navigate('Icons', iconPage)
-                }
-            ]
-        });
+        this.appMenuModel = new AppMenuModel();
         this.navigatorModel = new NavigatorModel({
             pageFactory: homePage,
             title: 'Toolbox'
--- client-app/src/mobile/home/HomePage.js
+++ client-app/src/mobile/home/HomePage.js
@@ -7,19 +7,52 @@
 
 import {Component} from 'react';
 import {HoistComponent, elemFactory} from '@xh/hoist/core';
+import {div} from '@xh/hoist/cmp/layout';
 import {page} from '@xh/hoist/mobile/cmp/page';
+import {button} from '@xh/hoist/mobile/cmp/button';
+import {Icon} from '@xh/hoist/icon';
 
-import {HomePageModel} from './HomePageModel';
+import './HomePage.scss';
+import {App} from '../App';
+import {gridPage} from '../grids/GridPage';
+import {iconPage} from '../icons/IconPage';
 
 @HoistComponent()
 export class HomePage extends Component {
-    localModel = new HomePageModel();
 
     render() {
-        // Todo: List of page summaries and buttons to visit each page
-        return page(
-            'Welcome to Toolbox'
-        );
+        return page({
+            cls: 'home-page',
+            items: [
+                this.renderSummaryCard({
+                    title: 'Grids',
+                    icon: Icon.grid({prefix: 'fal'}),
+                    summary: 'Show a collection of data bound to a store. Can specify 2 columns: leftColumn and rightColumn.',
+                    pageFactory: gridPage
+                }),
+                this.renderSummaryCard({
+                    title: 'Icons',
+                    icon: Icon.edit(),
+                    summary: 'A collection of FontAwesome SVG icons, available in 3 variants.',
+                    pageFactory: iconPage
+                })
+            ]
+        });
+    }
+
+    renderSummaryCard({title, icon, summary, pageFactory}) {
+        return div({
+            cls: 'summary-card',
+            items: [
+                div({cls: 'summary-card__title', item: title}),
+                div({cls: 'summary-card__body', item: summary}),
+                button({
+                    icon: icon,
+                    text: `Go to ${title}`,
+                    onClick: () => App.navigate(title, pageFactory)
+                })
+            ]
+        });
     }
 
 }
