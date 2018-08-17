d2c0fbf322b4416a88282d86ad6ec6677c063453
lbwexler <lbwexler@xh.io>
2018-08-17 16:06:39 -0400

    Update to PendingTaskModel, bit of a cleanup

--- client-app/src/desktop/tabs/other/LoadMaskPanel.js
+++ client-app/src/desktop/tabs/other/LoadMaskPanel.js
@@ -6,33 +6,34 @@
  */
 import React, {Component} from 'react';
 import {HoistComponent} from '@xh/hoist/core';
-import {wait, LastPromiseModel} from '@xh/hoist/promise';
+import {start, wait, PendingTaskModel} from '@xh/hoist/promise';
 import {observable, action, runInAction} from '@xh/hoist/mobx';
 import {box, filler} from '@xh/hoist/cmp/layout';
 import {numberField, textField, switchField} from '@xh/hoist/desktop/cmp/form';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
+
 import {loadMask} from '@xh/hoist/desktop/cmp/mask';
 import {toolbar} from '@xh/hoist/desktop/cmp/toolbar';
 import {button} from '@xh/hoist/desktop/cmp/button';
+import {SECONDS} from '@xh/hoist/utils/DateTimeUtils';
 import {wrapper, sampleGrid} from '../../common';
+
 
 @HoistComponent()
 export class LoadMaskPanel extends Component {
     @observable maskIsShown = false;
     @observable seconds = 3;
-    @observable maskText = '';
-    @observable dynamicText = false;
     @observable maskViewport = false;
 
-    promiseModel = new LastPromiseModel();
+    maskModel = new PendingTaskModel();
 
     render() {
-        const {maskText, maskViewport, maskIsShown, promiseModel} = this;
+        const {maskText, maskViewport, maskIsShown, maskModel} = this;
 
         return wrapper({
             description: <p>
-                LoadMask adds a spinner to the default mask component. It can display optional
-                text or dymanically relay messages from a linked PromiseModel. It can also be
+                LoadMask adds a spinner to the default mask component. It can optionally be
+                dynamically controlled by a linked PendingTaskModel. It can also be
                 placed over the entire viewport with <code>inline:false</code>.
             </p>,
             item: panel({
@@ -42,8 +43,7 @@ export class LoadMaskPanel extends Component {
                 items: [
                     sampleGrid({omitToolbar: true}),
                     loadMask({
-                        model: promiseModel,
-                        text: maskText,
+                        model: maskModel,
                         inline: !maskViewport
                     })
                 ],
@@ -55,25 +55,9 @@ export class LoadMaskPanel extends Component {
                             field: 'seconds',
                             width: 40,
                             min: 0,
-                            max: 10,
-                            disabled: this.dynamicText
+                            max: 10
                         }),
                         box('secs with'),
-                        textField({
-                            model: this,
-                            field: 'maskText',
-                            width: 120,
-                            placeholder: 'optional text',
-                            disabled: this.dynamicText
-                        }),
-                        switchField({
-                            model: this,
-                            field: 'dynamicText'
-                        }),
-                        box({
-                            className: 'xh-no-pad',
-                            item: 'dynamic text'
-                        }),
                         switchField({
                             model: this,
                             field: 'maskViewport'
@@ -96,37 +80,19 @@ export class LoadMaskPanel extends Component {
     }
 
     showMask = () => {
-        const {promiseModel} = this;
+        start(() => this.showMaskSequenceAsync()).linkTo(this.maskModel)
+    }
 
-        runInAction(() => this.maskIsShown = true);
-        if (this.dynamicText) {
-            promiseModel.setMessage('Loading...');
-            wait(1000)
-                .then(() => {
-                    promiseModel.setMessage('Still Loading...');
-                })
-                .wait(1000)
-                .then(() => {
-                    promiseModel.setMessage('Half way there...');
-                })
-                .wait(1000)
-                .then(() => {
-                    promiseModel.setMessage('Almost...');
-                })
-                .wait(1000)
-                .then(() => {
-                    promiseModel.setMessage('Done');
-                })
-                .wait(1000)
-                .thenAction(() => {
-                    this.maskIsShown = false;
-                    promiseModel.setMessage('');
-                })
-                .linkTo(promiseModel);
-            return;
-        }
-
-        wait(this.seconds * 1000).thenAction(() => this.maskIsShown = false).linkTo(this.promiseModel);
+    @action
+    async showMaskSequenceAsync() {
+        const maskModel = this.maskModel,
+            interval = this.seconds/3 * SECONDS;
+        maskModel.setMessage('Loading...');
+        await wait(interval);
+        maskModel.setMessage('Still Loading...');
+        await wait(interval);
+        maskModel.setMessage('Almost Finished...');
+        await wait(interval);
     }
 
     @action
@@ -143,11 +109,4 @@ export class LoadMaskPanel extends Component {
     setMaskViewport(maskViewport) {
         this.maskViewport = maskViewport;
     }
-
-    @action
-    setDynamicText(showDynamicText) {
-        this.dynamicText = showDynamicText;
-        if (showDynamicText) this.setSeconds(5);
-    }
-
 }
