b982bb587784900fd13149286f9341ac5f01dd34
Anselm McClain <atm@xh.io>
2018-08-20 10:25:42 -0700

    Version updates for release
    + Hoist React 12.0.0
    + Toolbox to 2.0-SNAPSHOT

--- client-app/webpack.config.js
+++ client-app/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = (env = {}) => {
     return configureWebpack({
         appCode: 'toolbox',
         appName: 'Toolbox',
-        appVersion: env.appVersion || '1.0-SNAPSHOT',
+        appVersion: env.appVersion || '2.0-SNAPSHOT',
         agGridLicenseKey: 'Extremely_Heavy_Industries_Scout_3Devs9_March_2019__MTU1MjA4OTYwMDAwMA==79f1a93b578543bf1e45a51272b2359a',
         favicon: './public/favicon.png',
         devServerOpenPage: 'app/',
