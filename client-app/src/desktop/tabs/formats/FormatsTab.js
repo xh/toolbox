651950069e21b887c9d76a4afc98ebd66941e446
shravnk <brendan.ode4@gmail.com>
2018-09-13 10:25:30 -0400

    - Improved formatting on Format Test component - fmtCompact is called from within fmtNumber if 'asCompact' flag is true - No zero padding allowed if in compact mode - Added units parameter to fmtNumber (fmtCompact mode only)

--- client-app/src/desktop/tabs/formats/FormatsTab.js
+++ client-app/src/desktop/tabs/formats/FormatsTab.js
@@ -16,7 +16,6 @@ import {
     selectField,
     textAreaField
 } from "@xh/hoist/desktop/cmp/form";
-import {button} from "@xh/hoist/desktop/cmp/button"
 
 import {formGroup} from "@xh/hoist/kit/blueprint";
 import {FormatsTabModel} from './FormatsTabModel';
@@ -34,104 +33,98 @@ export class FormatsTab extends Component {
             item:
                 panel({
                     title: 'Compact Number Formatter',
-                    className: 'toolbox-forms-tab',
+                    className: 'toolbox-formats-tab',
                     width: '90%',
                     height: '90%',
                     item: hframe({
                         items: [
-                            hframe({
+                            panel({
+                                title: 'Input',
+                                className: 'toolbox-formats-tab__panel',
+                                flex: 1,
+                                item: [
+                                    row({
+                                        field: 'testNumbers',
+                                        className: 'formats-text-area-input',
+                                        item: textAreaField({
+                                            style: {
+                                                textAlign: 'right',
+                                                height: '300px'
+                                            }
+                                        }),
+                                        info: 'Enter one number per line'
+                                    })
+                                ]
+                            }),
+                            panel({
+                                title: 'Parameters',
+                                className: 'toolbox-formats-tab__panel',
+                                flex: 1,
                                 items: [
-                                    panel({
-                                        className: 'toolbox-formats-tab__panel',
-                                        width: '50%',
-                                        items: [
-                                            row({
-                                            label: 'Test Numbers',
-                                            field: 'testNumbers',
-                                            item: textAreaField({
-                                                style: {
-                                                    height: '400px'
-                                                }
-                                            }),
-                                            display: false,
-                                            info: 'Comma Separated List'
-                                            }),
-                                            button({
-                                                text: 'Run Test'
-                                            })
-                                            ]
+                                    row({
+                                        label: 'Precision',
+                                        field: 'precision',
+                                        item: numberField({
+                                            min: 0,
+                                            max: 4,
+                                            selectOnFocus: true
+                                        }),
+                                        info: 'Max digits to the right of decimal place'
                                     }),
-                                    panel({
-                                        className: 'toolbox-formats-tab__panel',
-                                        width: '50%',
-                                        items: [
-                                            row({
-                                                label: 'Precision',
-                                                field: 'precision',
-                                                item: numberField({
-                                                    style: {
-                                                        width: '50%'
-                                                    }
-                                                }),
-                                                info: 'Max digits to the right of decimal place'
-                                            }),
-                                            row({
-                                                label: 'Decimal Tolerance',
-                                                field: 'decimalTolerance',
-                                                item: numberField({
-                                                    style: {
-                                                        width: '50%'
-                                                    }
-                                                }),
-                                                info: 'Number of decimal places tolerated before downsizing label. Should be less than or equal to precision'
-                                            }),
-                                            row({
-                                                label: 'Zero Pad',
-                                                field: 'zeroPad',
-                                                item: numberField({
-                                                    style: {
-                                                        width: '50%'
-                                                    }
-                                                }),
-                                                info: 'Zero padding applied where possible'
-                                            }),
-                                            row({
-                                                label: 'Units',
-                                                field: 'label',
-                                                item: selectField({
-                                                    options: model.SCALE_OPTIONS,
-                                                    placeholder: 'Select label...'
-                                                }),
-                                                info: 'Default: Apply scale to each number based on its size'
-                                            })
-                                        ]
+                                    row({
+                                        label: 'Decimal Tolerance',
+                                        field: 'decimalTolerance',
+                                        item: numberField({
+                                            value: model.decimalTolerance,
+                                            selectOnFocus: true
+                                        }),
+                                        info: 'Number of decimal places tolerated before downsizing units. Should be less than or equal to precision'
+                                    }),
+                                    row({
+                                        label: 'Zero Pad',
+                                        field: 'zeroPad',
+                                        item: numberField({
+                                            stepSize: 1,
+                                            min: 0,
+                                            max: 10,
+                                            selectOnFocus: true
+                                        }),
+                                        info: 'Zero padding applied where possible'
+                                    }),
+                                    row({
+                                        label: 'Units',
+                                        field: 'units',
+                                        item: selectField({
+                                            options: model.UNIT_OPTIONS,
+                                            placeholder: 'Select label...'
+                                        }),
+                                        info: 'Auto: Apply scale to each number based on its size'
                                     })
                                 ]
                             }),
                             panel({
                                 className: 'toolbox-formats-tab__panel',
                                 title: 'Results',
-                                width: '50%',
+                                flex: 1,
                                 item: [
 
-                                        <table>
+                                    <table>
 
-                                            <thead>
-                                                <tr>
-                                                    <th></th>
-                                                    <th>
-                                                        Input
-                                                    </th>
-                                                    <th>
-                                                        Output
-                                                    </th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {model.formattedNumbers}
-                                            </tbody>
-                                        </table>
-
+                                        <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th>
+                                                Input
+                                            </th>
+                                            <th>
+                                                Output
+                                            </th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {model.formattedNumbers}
+                                        </tbody>
+                                    </table>
                                 ]
                             })
                         ]
@@ -140,15 +133,15 @@ export class FormatsTab extends Component {
         });
     }
 
-    row = ({label, field, item, info, display=true}) => {
+    row = ({label, field, item, info}) => {
         const {model} = this;
 
         const formProps = {
-            label: label || capitalize(field),
+            label: label,
             item: React.cloneElement(item, {model, field}),
             helperText: info
         };
-        if (display) formProps.labelInfo = model[field];
+        formProps.labelInfo = model[field];
 
         return formGroup(formProps);
     }
--- client-app/src/desktop/tabs/formats/FormatsTabModel.js
+++ client-app/src/desktop/tabs/formats/FormatsTabModel.js
@@ -1,13 +1,13 @@
 import {HoistModel} from '@xh/hoist/core';
 import {bindable, computed} from '@xh/hoist/mobx';
-import {fmtCompact} from "@xh/hoist/format/FormatNumber";
+import {fmtNumber} from "@xh/hoist/format/FormatNumber";
 import React from "react";
 import {capitalize, toLower} from 'lodash';
 
 @HoistModel
 export class FormatsTabModel {
 
-    SCALE_OPTIONS = [
+    UNIT_OPTIONS = [
         'None',
         'Auto',
         'Thousands',
@@ -15,38 +15,34 @@ export class FormatsTabModel {
         'Billions'
     ];
 
-    @bindable testNumbers = "123456,\n" +
-                            "123450,\n" +
-                            "123400.1,\n" +
-                            "12456.12,\n" +
-                            "12345600,\n" +
-                            "12345000,\n" +
-                            "123456789.12,\n" +
-                            "123450000,\n" +
-                            "100000001,\n" +
-                            "0.25,\n" +
-                            "8,\n" +
-                            "101,\n" +
-                            "920120.21343,\n" +
-                            "1.224123";
+    @bindable testNumbers = "123456\n123450\n123400.1\n12456.12\n12345600\n12345000\n123456789.12\n" +
+                            "123450000\n100000001\n0.25\n101\n920120.21343\n1.224123";
 
     @bindable precision = 2;
     @bindable decimalTolerance = 2;
     @bindable zeroPad = 0;
-    @bindable label = 'Auto';
+    @bindable units = 'Auto';
 
-    @computed get fOptions() {
+    @computed
+    get fOptions() {
         return {
             asElement: true,
-            label: this.label === 'Default' ? null : toLower(this.label),
+            asCompact: true,
+            units: this.units === 'Auto' ? null : toLower(this.units),
             precision: this.precision,
             zeroPad: this.zeroPad,
             decimalTolerance: this.decimalTolerance
         }
     }
 
-    @computed get formattedNumbers() {
-        const sampleNumbers = this.testNumbers.split(",").map(v => parseFloat(v.trim()))
+    @computed
+    get sampleNumbers() {
+        return this.testNumbers.split("\n").map(v => parseFloat(v.trim()))
+    }
+
+    @computed
+    get formattedNumbers() {
+        const sampleNumbers = this.testNumbers.split("\n").map(v => parseFloat(v.trim()))
         return sampleNumbers.map(
             (num, index) =>
                 <tr key={`num-${index}`}>
@@ -55,7 +51,7 @@ export class FormatsTabModel {
                         {sampleNumbers[index]}
                     </td>
                     <td align="right">
-                        {fmtCompact(num, this.fOptions)}
+                        {fmtNumber(num, this.fOptions)}
                     </td>
                 </tr>
         );
