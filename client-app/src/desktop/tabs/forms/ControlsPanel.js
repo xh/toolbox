b98ace0bd895fb39ae7b5d147bb8adcfab01c3a6
febbraiod <donedesign1@gmail.com>
2018-09-14 11:49:39 -0400

    Fix regression with blur handling in baseDropdown

--- client-app/src/desktop/tabs/forms/ControlsPanel.js
+++ client-app/src/desktop/tabs/forms/ControlsPanel.js
@@ -166,7 +166,8 @@ export class ControlsPanel extends Component {
                                     field: 'option2',
                                     item: comboBox({
                                         options: movies,
-                                        placeholder: 'Search movies...'
+                                        placeholder: 'Search movies...',
+                                        requireSelection: true
                                     })
                                 }),
                                 row({
