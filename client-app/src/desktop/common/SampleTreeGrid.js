571c52ba2137dcc7a428a925a6b1fdf0cb0f016f
lbwexler <lbwexler@xh.io>
2018-09-26 10:42:41 -0400

    Cleanup mask

--- client-app/src/desktop/common/SampleTreeGrid.js
+++ client-app/src/desktop/common/SampleTreeGrid.js
@@ -84,7 +84,7 @@ class SampleTreeGrid extends Component {
             className: this.getClassName(),
             ...this.getLayoutProps(),
             item: grid({model}),
-            mask: mask({spinner: true, model: this.loadModel}),
+            mask: this.loadModel,
             bbar: toolbar({
                 omit: this.props.omitToolbar,
                 items: [
