6193b7eed8f4a6fea62094db1c783f5b8aad139d
Anselm McClain <atm@xh.io>
2018-10-21 21:42:01 -0700

    Checkbox updates
    + Take name change -> Checkbox.
    + Rename and fix SampleTreeWithCheckboxGrid - need to use new structured API vs. relying on old behavior of passing props direct to BP.

--- client-app/src/desktop/tabs/forms/ControlsPanel.js
+++ client-app/src/desktop/tabs/forms/ControlsPanel.js
@@ -14,7 +14,7 @@ import moment from 'moment';
 import {fmtDateTime, fmtThousands} from '@xh/hoist/format';
 import {
     formField,
-    checkBox,
+    checkbox,
     dateInput,
     textInput,
     textArea,
@@ -232,7 +232,7 @@ export class ControlsPanel extends Component {
                                 row({
                                     label: 'CheckBox',
                                     field: 'bool1',
-                                    item: checkBox({
+                                    item: checkbox({
                                         label: 'enabled'
                                     })
                                 }),
