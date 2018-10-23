d0aee8148d770483b1c1ecd94394fa317f8cfeb0
Anselm McClain <atm@xh.io>
2018-10-23 13:14:03 -0700

    Simplifications to App.js layout

--- client-app/src/desktop/App.js
+++ client-app/src/desktop/App.js
@@ -8,7 +8,7 @@
 import {Component} from 'react';
 import {HoistComponent} from '@xh/hoist/core';
 import {tabContainer, tabSwitcher} from '@xh/hoist/desktop/cmp/tab';
-import {vframe, frame} from '@xh/hoist/cmp/layout';
+import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {Icon} from '@xh/hoist/icon';
 import {appBar} from '@xh/hoist/desktop/cmp/appbar';
 import {ContextMenuSupport, ContextMenuItem} from '@xh/hoist/desktop/cmp/contextmenu';
@@ -20,39 +20,28 @@ import './App.scss';
 export class App extends Component {
 
     render() {
-        return vframe(
-            this.renderNavBar(),
-            this.renderBody()
-        );
+        const {tabModel} = this.model;
+
+        return panel({
+            tbar: appBar({
+                icon: Icon.boxFull({size: '2x', prefix: 'fal'}),
+                title: 'Hoist React Toolbox',
+                leftItems: [
+                    tabSwitcher({model: tabModel})
+                ],
+                hideRefreshButton: true
+            }),
+            className: 'toolbox-app-frame',
+            item: tabContainer({
+                model: tabModel,
+                switcherPosition: 'none'
+            })
+        });
     }
 
     getContextMenuItems() {
         const Item = ContextMenuItem;
         return [Item.reloadApp(), Item.about(), Item.logout()];
     }
-
-    //------------------
-    // Implementation
-    //------------------
-    renderNavBar() {
-        return appBar({
-            icon: Icon.boxFull({size: '2x'}),
-            title: 'Hoist React Toolbox',
-            leftItems: [
-                tabSwitcher({model: this.model.tabModel})
-            ],
-            hideRefreshButton: true
-        });
-    }
-
-    renderBody() {
-        return frame({
-            className: 'toolbox-app-frame',
-            item: tabContainer({
-                model: this.model.tabModel,
-                switcherPosition: 'none'
-            })
-                });
-            }
 
 }
