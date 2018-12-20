2144fc5a2dcfb2bf23f1186ce0cda6240256dab5
Lee Wexler <lbwexler@xh.io>
2018-12-20 17:47:33 -0500

    Adapt to new Forms model (#135)

--- client-app/src/mobile/form/FormPage.js
+++ client-app/src/mobile/form/FormPage.js
@@ -1,9 +1,11 @@
 import {Component} from 'react';
 import {HoistComponent, elemFactory} from '@xh/hoist/core';
-import {div} from '@xh/hoist/cmp/layout';
+import {div, vframe, span, filler} from '@xh/hoist/cmp/layout';
 import {page} from '@xh/hoist/mobile/cmp/page';
+import {toolbar} from '@xh/hoist/mobile/cmp/toolbar';
 import {button} from '@xh/hoist/mobile/cmp/button';
 import {Icon} from '@xh/hoist/icon';
+import {form} from '@xh/hoist/cmp/form';
 import {
     formField,
     label,
@@ -22,93 +24,107 @@ import {FormPageModel} from './FormPageModel';
 
 @HoistComponent
 export class FormPage extends Component {
+
     model = new FormPageModel();
 
     render() {
-        const {model} = this;
         return page({
             className: 'toolbox-page form-page',
             items: [
-                div({
-                    className: 'toolbox-card',
+                vframe({
+                    overflowY: 'auto',
                     items: [
-                        formField({
-                            model,
-                            field: 'name',
-                            item: textInput()
-                        }),
-                        formField({
-                            model,
-                            field: 'movie',
-                            item: select({options: model.movies})
-                        }),
-                        formField({
-                            model,
-                            field: 'salary',
-                            item: numberInput({
-                                enableShorthandUnits: true,
-                                displayWithCommas: true
-                            })
-                        }),
-                        formField({
-                            model,
-                            field: 'included',
-                            item: checkbox()
-                        }),
-                        formField({
-                            model,
-                            field: 'enabled',
-                            item: switchInput()
-                        }),
-                        formField({
-                            model,
-                            field: 'buttonGroup',
-                            item: buttonGroupInput(
-                                button({
-                                    text: 'Button 1',
-                                    value: 'button1'
-                                }),
-                                button({
-                                    icon: Icon.moon(),
-                                    value: 'button2'
-                                }),
-                                button({
-                                    icon: Icon.skull(),
-                                    text: 'Button 2',
-                                    value: 'button3'
-                                })
-                            )
-                        }),
-                        formField({
-                            model,
-                            field: 'notes',
-                            item: textArea()
-                        }),
-                        formField({
-                            model,
-                            field: 'searchQuery',
-                            item: searchInput()
-                        })
+                        this.renderForm(),
+                        this.renderResults()
                     ]
                 }),
-                div({
-                    className: 'toolbox-card',
-                    items: [
-                        this.renderResult('Name:', 'name'),
-                        this.renderResult('Movie:', 'movie'),
-                        this.renderResult('Salary:', 'salary'),
-                        this.renderResult('Selected Button:', 'buttonGroup'),
-                        this.renderResult('Notes:', 'notes'),
-                        this.renderResult('Search:', 'searchQuery')
-                    ]
-                })
+                this.renderToolbar()
+            ]
+        });
+    }
+
+    renderForm() {
+        const {model} = this,
+            {formModel, minimal, readonly, movies} = model;
+
+        return div({
+            className: 'toolbox-card',
+            items: form({
+                model: formModel,
+                fieldDefaults: {minimal, readonly},
+                flexDirection: 'column',
+                items: [
+                    formField({
+                        field: 'name',
+                        info: 'Min. 8 chars',
+                        item: textInput()
+                    }),
+                    formField({
+                        field: 'movie',
+                        item: select({options: movies})
+                    }),
+                    formField({
+                        field: 'salary',
+                        item: numberInput({
+                            enableShorthandUnits: true,
+                            displayWithCommas: true
+                        })
+                    }),
+                    formField({
+                        field: 'included',
+                        item: checkbox()
+                    }),
+                    formField({
+                        field: 'enabled',
+                        item: switchInput()
+                    }),
+                    formField({
+                        field: 'buttonGroup',
+                        item: buttonGroupInput(
+                            button({
+                                text: 'Button 1',
+                                value: 'button1'
+                            }),
+                            button({
+                                icon: Icon.moon(),
+                                value: 'button2'
+                            }),
+                            button({
+                                icon: Icon.skull(),
+                                text: 'Button 2',
+                                value: 'button3'
+                            })
+                        )
+                    }),
+                    formField({
+                        field: 'notes',
+                        item: textArea()
+                    }),
+                    formField({
+                        field: 'searchQuery',
+                        item: searchInput()
+                    })
+                ]
+            })
+        });
+    }
+
+    renderResults() {
+        return div({
+            className: 'toolbox-card',
+            items: [
+                this.renderResult('Name:', 'name'),
+                this.renderResult('Movie:', 'movie'),
+                this.renderResult('Salary:', 'salary'),
+                this.renderResult('Selected Button:', 'buttonGroup'),
+                this.renderResult('Notes:', 'notes'),
+                this.renderResult('Search:', 'searchQuery')
             ]
         });
     }
 
     renderResult(labelText, field) {
         const value = this.model[field];
-
         return div({
             className: 'form-field-result',
             items: [
@@ -118,6 +134,16 @@ export class FormPage extends Component {
         });
     }
 
+    renderToolbar() {
+        const {model} = this;
+        return toolbar(
+            switchInput({model, field: 'readonly'}),
+            span('Read-only'),
+            filler(),
+            switchInput({model, field: 'minimal'}),
+            span('Minimal validation'),
+        );
+    }
 }
 
 export const formPage = elemFactory(FormPage);
