0e65b0cf3cabca2adaf55007b82716f671db3d38
Anselm McClain <atm@xh.io>
2019-01-18 11:38:40 -0800

    WIP server-side support for file upload example

---
+++ grails-app/controllers/io/xh/toolbox/files/FileManagerController.groovy
@@ -0,0 +1,37 @@
+package io.xh.toolbox.files
+
+import io.xh.hoist.security.Access
+import io.xh.toolbox.BaseController
+
+@Access(['HOIST_ADMIN'])
+class FileManagerController extends BaseController {
+
+    def fileManagerService
+
+    def upload() {
+        fileManagerService.saveFromRequest(request)
+        renderJSON(success: true)
+    }
+
+    def list() {
+        def ret = fileManagerService.list().collect{
+            [
+                name: it.name,
+                path: it.path,
+                size: it.size()
+            ]
+        }
+        renderJSON(ret)
+    }
+
+    def delete(String filename) {
+        def success = fileManagerService.delete(filename)
+        renderJSON(success: success)
+    }
+
+    def deleteAll() {
+        def ret = fileManagerService.deleteAll()
+        renderJSON(ret)
+    }
+
+}
