67f883c807b8b11d58cec9232bfc59dd5c984d69
lbwexler <lbwexler@xh.io>
2019-04-26 11:24:30 -0400

    Checkpoint -- simplification of cube code

--- client-app/src/desktop/tabs/grids/CubeDataModel.js
+++ client-app/src/desktop/tabs/grids/CubeDataModel.js
@@ -1,148 +1,120 @@
-import {XH, HoistModel, managed} from '@xh/hoist/core';
+import {HoistModel, managed, XH} from '@xh/hoist/core';
 import {LoadSupport} from '@xh/hoist/core/mixins';
-import {millionsRenderer, numberRenderer} from '@xh/hoist/format';
 import {GridModel, emptyFlexCol} from '@xh/hoist/cmp/grid';
-import {random, sample, times} from 'lodash';
-import {start} from '@xh/hoist/promise';
-import {bindable, observable, action} from '@xh/hoist/mobx'
 import {View} from '@xh/hoist/data/cube';
 
-const pnlColumn = {
-    absSort: true,
-    align: 'right',
-    width: 120,
-    renderer: numberRenderer({
-        precision: 0,
-        ledger: true,
-        colorSpec: true,
-        tooltip: true
-    })
-};
+import {Cube} from '@xh/hoist/data/cube';
+import {DimensionChooserModel} from '@xh/hoist/desktop/cmp/dimensionchooser';
+import {numberRenderer, millionsRenderer, fmtNumberTooltip} from '@xh/hoist/format';
 
 @HoistModel
 @LoadSupport
 export class CubeDataModel {
 
-
     @managed
     gridModel = this.createGridModel();
 
     @managed
-    cubeModel = new CubeModel();
-
+    cube = new Cube({
+        idSpec: XH.genId,
+        fields: [
+            {name: 'symbol', isDimension: true},
+            {name: 'sector', isDimension: true},
+            {name: 'model', isDimension: true},
+            {name: 'fund', isDimension: true},
+            {name: 'region', isDimension: true},
+            {name: 'trader', isDimension: true},
+            {name: 'mktVal', isDimension: false, aggregator: 'SUM'},
+            {name: 'pnl', isDimension: false, aggregator: 'SUM'}
+        ]
+    });
+    
+    @managed
     cubeView = new View({
-        cube: this.cubeModel.cube,
-        query: {},
+        cube: this.cube,
+        boundStore: this.gridModel.store,
+        query: {
+            filters: [],
+            dimensions: ['fund'],
+            includeRoot: false,
+            includeLeaves: true
+        },
         connect: true
     });
 
-    @observable.ref runTimes = {};
+    @managed
+    dimChooserModel = new DimensionChooserModel({
+        dimensions: [
+            {value: 'fund', label: 'Fund'},
+            {value: 'model', label: 'Model'},
+            {value: 'region', label: 'Region'},
+            {value: 'sector', label: 'Sector'},
+            {value: 'symbol', label: 'Symbol'},
+            {value: 'trader', label: 'Trader'}
+        ]
+    });
 
-    constructor() {
-        const store = this.gridModel.store;
-        this.cubeView.setBoundStore(store);
+    async doLoadAsync() {
+        const positions = await XH.portfolioService.getPositionsAsync();
+        this.cube.loadData(positions, {});
+
+        window.cube = this.cube;
+        window.cubeView = this.cubeView;
     }
-
-    @action
-    setRunTimes(v) {this.runTimes = v}
-
-    loadCube() {
-        this.cubeModel.reloadCube();
-    }
-
 
     createGridModel() {
         return new GridModel({
-            selModel: {mode: 'multiple'},
-            sortBy: 'day|desc|abs',
+            treeMode: true,
+            sortBy: 'pnl|desc|abs',
             emptyText: 'No records found...',
+            enableColChooser: true,
+            enableExport: true,
+            rowBorders: true,
+            showHover: true,
+            compact: XH.appModel.useCompactGrids,
             columns: [
                 {
                     field: 'id',
                     headerName: 'ID',
-                    width: 140,
+                    width: 40,
+                    hidden: true
                 },
                 {
-                    field: 'symbol',
-                    width: 200
+                    field: 'cubeLabel',
+                    headerName: 'Name',
+                    flex: 1,
+                    minWidth: 180,
+                    isTreeColumn: true
                 },
                 {
-                    headerName: 'Volume',
-                    field: 'volume',
+                    field: 'mktVal',
+                    headerName: 'Mkt Value (m)',
+                    headerTooltip: 'Market value (in millions USD)',
                     align: 'right',
                     width: 130,
+                    absSort: true,
+                    tooltip: (val) => fmtNumberTooltip(val, {ledger: true}),
                     renderer: millionsRenderer({
-                        precision: 2,
-                        label: true,
-                        tooltip: true
+                        precision: 3,
+                        ledger: true
                     })
                 },
-                {...emptyFlexCol}
+                {
+                    field: 'pnl',
+                    headerName: 'P&L',
+                    align: 'right',
+                    width: 130,
+                    absSort: true,
+                    tooltip: (val) => fmtNumberTooltip(val, {ledger: true}),
+                    renderer: numberRenderer({
+                        precision: 0,
+                        ledger: true,
+                        colorSpec: true
+                    })
+                }, {
+                    ...emptyFlexCol
+                }
             ]
         });
     }
-
-
-}
-
-import {Cube} from '@xh/hoist/data/cube';
-
-class CubeModel {
-
-    @bindable recordCount = 25000;
-    @bindable idSeed = 1;
-
-    cube = new Cube({
-        data: this.genTestData(),
-        fields: [{name: 'id'}, {name: 'symbol;'}, {name: 'volume'}],
-    });
-
-
-    reloadCube() {
-        this.cube.loadData(this.genTestData());
-    }
-
-
-    //----------------
-    // IMPLEMENTATION
-    //----------------
-
-    genTestData() {
-        let count = 0;
-        const idSeed = this.idSeed;
-        let ret = [];
-        while (count < this.recordCount) {
-            let symbol = 'Symbol ' + count;
-
-            count++;
-            const pos = {
-                id: `${idSeed}~${symbol}`,
-                symbol,
-                volume: random(1000, 2000000)
-            };
-            ret.push(pos);
-            // if (this.tree) {
-            //     const childCount = random(0, 10);
-            //     pos.children = times(childCount, (t) => {
-            //         trader = 'trader' + t;
-            //         count++;
-            //         const child = {
-            //             id: `${idSeed}~${symbol}~${trader}`,
-            //             trader,
-            //             symbol,
-            //             day: random(-80000, 100000),
-            //             mtd: random(-500000, 500000),
-            //             ytd: random(-1000000, 2000000),
-            //             volume: random(1000, 1200000)
-            //         };
-            //         this._allData.push(child);
-            //         return child;
-            //     });
-            // }
-
-        }
-        console.log(`Generated ${count} test records.`);
-        return ret;
-    }
-
 }
--- client-app/src/desktop/tabs/grids/CubeDataPanel.js
+++ client-app/src/desktop/tabs/grids/CubeDataPanel.js
@@ -1,15 +1,11 @@
-import {filler, span} from '@xh/hoist/cmp/layout';
-import {fmtNumber} from '@xh/hoist/format';
 import {Component} from 'react';
 import {HoistComponent} from '@xh/hoist/core';
 import {Icon} from '@xh/hoist/icon';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {storeFilterField} from '@xh/hoist/desktop/cmp/store';
-import {toolbar, toolbarSep} from '@xh/hoist/desktop/cmp/toolbar';
-import {numberInput, switchInput} from '@xh/hoist/desktop/cmp/input';
+import {toolbar} from '@xh/hoist/desktop/cmp/toolbar';
 import {button} from '@xh/hoist/desktop/cmp/button';
 import {grid} from '@xh/hoist/cmp/grid';
-import {tooltip} from '@xh/hoist/kit/blueprint';
 
 import {CubeDataModel} from './CubeDataModel';
 
@@ -24,57 +20,17 @@ export class CubeDataPanel extends Component {
 
         return panel({
             mask: model.loadModel,
-            item: gridModel ? grid({
-                key: gridModel.xhId,
-                model: gridModel,
-                renderFlag: model.renderFlag
-            }) : null,
+            item: grid({model: gridModel}),
             tbar: toolbar(
-                tooltip({
-                    content: 'ID prefix',
-                    item: numberInput({
-                        model,
-                        bind: 'idSeed',
-                        width: 40
-                    })
-                }),
-                tooltip({
-                    content: '# records to generate',
-                    item: numberInput({
-                        model,
-                        bind: 'recordCount',
-                        width: 100
-                    })
-                }),
                 button({
                     text: 'Reload cube',
                     icon: Icon.gears(),
-                    onClick: () => model.loadCube()
-                }),
-                toolbarSep(),
-                switchInput({
-                    model,
-                    bind: 'tree',
-                    label: 'Tree mode',
-                    labelAlign: 'left'
-                }),
-                toolbarSep(),
-                filler(),
-                span(this.formatRunTimes())
-            ),
-            bbar: toolbar([
-                storeFilterField({
-                    includeFields: ['symbol', 'trader'],
-                    gridModel
+                    onClick: () => model.loadAsync()
                 })
-            ])
+            ),
+            bbar: toolbar(
+                storeFilterField({gridModel})
+            )
         });
-    }
-
-    formatRunTimes() {
-        const rt = this.model.runTimes,
-            fmt = (v) => v ? fmtNumber(v, {precision: 0, label: 'ms', labelCls: null}) : 'N/A';
-
-        return `Gen Data: ${fmt(rt.data)} • Load: ${fmt(rt.load)} `;
     }
 }
