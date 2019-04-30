3d6cb64da2bc181bffef9bbaa38f5d6036891476
YanaNirvana <yana@xh.io>
2019-04-30 09:33:24 -0400

    added comments to help remember

--- grails-app/services/io/xh/toolbox/app/RecallsService.groovy
+++ grails-app/services/io/xh/toolbox/app/RecallsService.groovy
@@ -14,9 +14,10 @@ class RecallsService extends BaseService {
 
 
     List fetchRecalls() {
-        def host = configService.getString('recallsHost')
-
-        def url = new URL("https://api.fda.gov/drug/enforcement.json?search=_exists_:openfda&sort=recall_initiation_date:desc&limit=99"),
+        def host = configService.getString('recallsHost'),
+            url = new URL("https://api.fda.gov/drug/enforcement.json?search=" +
+                    "_exists_:openfda&sort=recall_initiation_date:desc&limit=99"
+            ),
             response = JSON.parse(url.openStream(), 'UTF-8')
 
         return response.results
@@ -25,14 +26,17 @@ class RecallsService extends BaseService {
     List fetchSearch(drugName) {
         def host = configService.getString('recallsHost'),
             url = new URL("https://api.fda.gov/drug/enforcement.json?search=" +
-                    "openfda.generic_name:${drugName}+openfda.brand_name:${drugName}&sort=recall_initiation_date:desc&limit=99"),
+                    "openfda.generic_name:${drugName}+openfda.brand_name:${drugName}" +
+                    "&sort=recall_initiation_date:desc&limit=99"
+            ),
             connection = url.openConnection()
 
+
         if (connection.responseCode == 404) {
-            return [];
+            return [];  // return empty if none found
         } else {
             return JSON.parse(connection.getInputStream(), 'UTF-8').results
-        }
+        }               // return everything else, including exceptions
     }
 
     def configService
