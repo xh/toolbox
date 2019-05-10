b2c2d6feec1fb709e3ea68563a7599f8a149966a
YanaNirvana <yana@xh.io>
2019-05-10 09:41:57 -0400

    buttons for popups are working but an alert / confirm are just special messages?

--- client-app/src/desktop/tabs/other/PopupsPanel.js
+++ client-app/src/desktop/tabs/other/PopupsPanel.js
@@ -6,15 +6,86 @@
  */
 
 import {Component} from 'react';
-import {HoistComponent} from '@xh/hoist/core';
+import {XH, HoistComponent} from '@xh/hoist/core';
 import {wrapper} from '../../common';
-import {p} from '@xh/hoist/cmp/layout';
+import {p, div} from '@xh/hoist/cmp/layout';
+import {button} from '@xh/hoist/desktop/cmp/button';
 
 
 @HoistComponent
 export class PopupsPanel extends Component {
 
     render() {
-        return wrapper('hi');
+
+        const buttonAppearance = {
+            // icon: Icon.add(),
+            minimal: false,
+            flex: 1,
+            margin: 5
+        };
+
+        return wrapper({
+            description: p('here are some popups'),
+            items: [
+                div(
+                    button({
+                        ...buttonAppearance,
+                        text: 'Alert',
+                        onClick: () => XH.alert({
+                            title: 'Aawwwwww',
+                            message: 'this is an alert dialog.'
+                        })
+                    }),
+                    button({
+                        ...buttonAppearance,
+                        text: 'Confirm',
+                        onClick: () => XH.confirm({
+                            title: 'Confirm',
+                            message: 'This is a confirm dialoge.'
+                        })
+                    }),
+                    button({
+                        ...buttonAppearance,
+                        text: 'Message (alert)',
+                        onClick: () => XH.alert({
+                            title: 'Message',
+                            message: 'Messages are highly configurable.',
+                            confirmText: 'Ok, got it',
+                            cancelText: 'Exit'
+                        })
+                    }),
+                    button({
+                        ...buttonAppearance,
+                        text: 'Message (confirm)',
+                        onClick: () => XH.confirm({
+                            title: 'Message',
+                            message: 'Messages are highly configurable.',
+                            confirmText: 'Ok, got it',
+                            cancelText: 'Exit'
+                        })
+                    }),
+                    button({
+                        ...buttonAppearance,
+                        text: 'Message (message)',
+                        onClick: () => XH.message({
+                            title: 'Message',
+                            message: 'Messages are highly configurable.',
+                            confirmText: 'Ok, got it',
+                            cancelText: 'Exit'
+                        })
+                    }),
+
+                ),
+                div(
+                    button({
+                        ...buttonAppearance,
+                        text: 'Toast',
+                        onClick: () => XH.toast({
+                            message: 'This is a toast.'
+                        })
+                    })
+                )
+            ]
+        });
     }
 }
