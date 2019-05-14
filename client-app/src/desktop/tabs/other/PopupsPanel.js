d55d8ccfdda53e55ddcc672c3078129ab4bd9aa9
YanaNirvana <yana@xh.io>
2019-05-14 09:59:14 -0400

    checkpoint...

--- client-app/src/desktop/tabs/other/PopupsPanel.js
+++ client-app/src/desktop/tabs/other/PopupsPanel.js
@@ -10,7 +10,6 @@ import {XH, HoistComponent} from '@xh/hoist/core';
 import {wrapper} from '../../common';
 import {p, div, table, tr, td, tbody, vbox, code, span, ul, li} from '@xh/hoist/cmp/layout';
 import {button} from '@xh/hoist/desktop/cmp/button';
-import {Icon} from '@xh/hoist/icon';
 import {Ref} from '@xh/hoist/utils/react';
 
 import './PopupsPanel.scss';
@@ -24,12 +23,12 @@ export class PopupsPanel extends Component {
 
         const buttonAppearance = {
             className: 'toolbox-popups-button',
-            icon: Icon.add(),
+            // icon: Icon.add(),
             minimal: false,
             flex: 1,
             margin: 5
         };
-        const row = (col1, col2, col3, col4) => {
+        const row = (col1, col2, col3) => {
             return tr(
                 td(col1), td(col2), td(col3)
             );
@@ -41,7 +40,6 @@ export class PopupsPanel extends Component {
                 className: 'toolbox-popups-vframe',
                 ref: this.divRef.ref,
                 item: table({
-                    // ref: this.divRef.ref,
                     item: tbody(
                         row(
                             button({
@@ -54,13 +52,26 @@ export class PopupsPanel extends Component {
                             }),
                             button({
                                 ...buttonAppearance,
-                                text: 'Alert (customized) ',
+                                text: 'Alert w/ custom Button',
                                 onClick: () => XH.alert({
                                     title: 'Alert Title',
                                     message: 'This is also an alert.  Here, we customized the text in the button.',
                                     confirmText: 'COOL BUTTON!'
 
                                 })
+                            }),
+                            button({
+                                ...buttonAppearance,
+                                text: 'Alert w/ callback',
+                                onClick: () => XH.alert({
+                                    title: 'Alert Title',
+                                    message: p('Alerts return a promise that resolves to ', code('true'),
+                                        'when user acknowledges the Alert'
+                                    )
+                                }).then(() => XH.toast({
+                                    message: p('Oh look, a ', code('.then')),
+                                    containerRef: this.divRef.value
+                                }))
                             })
                         ),
                         row(
@@ -74,7 +85,7 @@ export class PopupsPanel extends Component {
                             }),
                             button({
                                 ...buttonAppearance,
-                                text: 'Confirm (customized)',
+                                text: 'Confirm w/ custom Button',
                                 onClick: () => XH.confirm({
                                     title: 'Confirm Title',
                                     message: 'This is also a confirm. Like Alerts, we can also customize ' +
@@ -82,6 +93,18 @@ export class PopupsPanel extends Component {
                                     confirmText: 'Nice',
                                     cancelText: 'Nope'
                                 })
+                            }),
+                            button({
+                                ...buttonAppearance,
+                                text: 'Confirm w/ callback',
+                                onClick: () => XH.confirm({
+                                    title: 'Confirm Title',
+                                    message: p('Confirms promises resolve to ', code('true'), ' if user confirms, or ',
+                                        code('false'), ' if user cancels.')
+                                }).then((returnedBoolean) => XH.toast({
+                                    message: p('That promise resolved to ', code(`${returnedBoolean}`)),
+                                    containerRef: this.divRef.value
+                                }))
                             })
                         ),
                         row(
@@ -92,7 +115,8 @@ export class PopupsPanel extends Component {
                                     title: 'Message Title',
                                     message: div(
                                         p('Messages are highly configurable. (Alerts and Confirms are just preconfigured Messages).'),
-                                        p('Dev Beware: without ', code('confirmText'), ' or ', code('cancelText'), ' there will be no buttons!')
+                                        p('Dev Beware: without', code('confirmText'), ' or ', code('cancelText'),
+                                            ' Messages will have no buttons!')
                                     ),
                                     confirmText: 'Oh I see!',
                                     cancelText: 'Nope, no seas here.'
@@ -100,22 +124,47 @@ export class PopupsPanel extends Component {
                             }),
                             button({
                                 ...buttonAppearance,
-                                text: 'Message (advanced)',
+                                text: 'Message w/ adv configs',
+                                onClick: () => {
+                                    XH.message({
+                                        title: 'Message Title',
+                                        message: p(
+                                            'This is a message.  You can also pass a function to Message, Alert, and Confirm via the ',
+                                            code('onCancel'), ' or ', code('onConfirm'), ' config.'
+                                        ),
+                                        confirmText: 'Got it',
+                                        cancelText: 'Come again?',
+                                        onConfirm: () => XH.toast({
+                                            message: p(code('onConfirm'), ' is an alternative to ', code('.then')),
+                                            containerRef: this.divRef.value
+                                        }),
+                                        onCancel: () => XH.toast({
+                                            message: p(code('onCancel'), ' is an alternative to ', code('.then')),
+                                            containerRef: this.divRef.value
+                                        }),
+                                    })
+                                }
+                            }),
+                            button({
+                                ...buttonAppearance,
+                                text: 'Message w/ callback',
                                 onClick: () => {
                                     XH.message({
                                         title: 'Message Title',
                                         message: div(
                                             p('Messages, Alerts, and Confirms all return a promise... \n\n'),
                                             ul(
-                                                li('Alert promises return true when user acknolwedges alert. '),
-                                                li('Confirm and Message promises return true if user confirms, ' +
-                                                'or false if user cancels.')
-                                            ),
-                                            p('The return of this promise will be ', code('console.log()'), ' ed')
+                                                li('Alert promises resolve to true when user acknolwedges alert. '),
+                                                li('Confirm and Message promises resolve to true if user confirms, ' +
+                                                    'or false if user cancels.')
+                                            )
                                         ),
-                                        confirmText: 'OK',
+                                        confirmText: 'Cool',
                                         cancelText: 'Cancel'
-                                    }).then(bool => console.log('The promise returned: ', bool));
+                                    }).then((returnedBoolean) => XH.toast({
+                                        message: p('That promise resolved to ', code(`${returnedBoolean}`)),
+                                        containerRef: this.divRef.value
+                                    }))
                                 }
                             })
                         ),
@@ -124,7 +173,7 @@ export class PopupsPanel extends Component {
                                 ...buttonAppearance,
                                 text: 'Toast',
                                 onClick: () => XH.toast({
-                                    message: 'This is a toast.'
+                                    message: 'This is a toast. Bottom right by default.'
                                 })
                             }),
                             button({
