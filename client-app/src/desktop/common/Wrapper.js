42734e0ed1c0bfb5249aaae28a59f9a64dab286e
YanaNirvana <yana@xh.io>
2019-06-03 17:02:44 -0400

    first attempt: works for single link!

--- client-app/src/desktop/common/Wrapper.js
+++ client-app/src/desktop/common/Wrapper.js
@@ -1,21 +1,33 @@
 import {Component} from 'react';
 import {HoistComponent, elemFactory} from '@xh/hoist/core';
-import {box} from '@xh/hoist/cmp/layout';
+import {box, a, br} from '@xh/hoist/cmp/layout';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
+import {Icon} from '@xh/hoist/icon';
 
 import './Wrapper.scss';
 
 @HoistComponent
 class Wrapper extends Component {
     render() {
-        const {description, children, ...rest} = this.props;
-
+        const {description, link, children, ...rest} = this.props,
+            viewSource = link ?
+                a({
+                    href: link,
+                    item: Icon.code(),
+                    target: '_blank'
+                }) :
+                null;
+        
         return box({
             className: 'toolbox-wrapper xh-tiled-bg',
             items: [
                 panel({
                     className: 'toolbox-wrapper-description',
-                    item: description,
+                    items: [
+                        description,
+                        br(),
+                        viewSource
+                    ],
                     width: 700,
                     marginBottom: 10,
                     omit: !description
