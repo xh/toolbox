17324fa66153e2ca5b114a43b54182c8b8829bee
YanaNirvana <yana@xh.io>
2019-06-04 15:32:44 -0400

    second attempt: works for multiple links...

--- client-app/src/desktop/common/Wrapper.js
+++ client-app/src/desktop/common/Wrapper.js
@@ -1,6 +1,6 @@
 import {Component} from 'react';
 import {HoistComponent, elemFactory} from '@xh/hoist/core';
-import {box, a, br} from '@xh/hoist/cmp/layout';
+import {box, a, br, code, div, p, span} from '@xh/hoist/cmp/layout';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {Icon} from '@xh/hoist/icon';
 
@@ -8,34 +8,61 @@ import './Wrapper.scss';
 
 @HoistComponent
 class Wrapper extends Component {
+    
     render() {
-        const {description, link, children, ...rest} = this.props,
-            viewSource = link ?
-                a({
-                    href: link,
-                    item: Icon.code(),
-                    target: '_blank'
-                }) :
-                null;
+        const {description, children, ...rest} = this.props;
         
         return box({
             className: 'toolbox-wrapper xh-tiled-bg',
             items: [
                 panel({
                     className: 'toolbox-wrapper-description',
-                    items: [
-                        description,
-                        br(),
-                        viewSource
-                    ],
+                    item: description,
                     width: 700,
                     marginBottom: 10,
                     omit: !description
                 }),
-                children
+                children,
+                panel({
+                    item: this.generateLinks(),
+                    icon: Icon.code(),
+                    title: 'Source Code',
+                    model: {
+                        defaultSize: 100,
+                        collapsible: true,
+                        showSplitterCollapseButton: false,
+                        side: 'bottom',
+                        hide: true
+                    }
+                })
             ],
             ...rest
         });
     }
+    
+    generateLinks() {
+        const {links, link} = this.props;
+        if (Array.isArray(links)) {
+            return div(
+                links.map(this.generateSingleLink)
+            );
+        } else if (link) {
+            return this.generateSingleLink(link);
+        }
+        return null;
+    }
+    
+    generateSingleLink(linkObj) {
+        return p(
+            a({
+                href: linkObj.url,
+                item: code(linkObj.text),
+                target: '_blank'
+            }),
+            ' ',
+            linkObj.notes
+        );
+    }
+    
 }
 export const wrapper = elemFactory(Wrapper);
