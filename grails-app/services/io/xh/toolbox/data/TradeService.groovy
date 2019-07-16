eac865fc25e4aff6da653eef6539d61267c0d151
inewell <inewell@andover.edu>
2019-07-16 10:42:24 -0400

    Move TradeService to server side

---
+++ grails-app/services/io/xh/toolbox/data/TradeService.groovy
@@ -0,0 +1,41 @@
+package io.xh.toolbox.data
+
+import groovy.json.JsonSlurper
+import io.xh.hoist.BaseService
+
+class TradeService extends BaseService {
+
+    private Map trades = loadTradesFromFile()
+
+    Map getTrades() {
+        return trades
+    }
+
+    //------------------------
+    // Implementation
+    //------------------------
+
+    private Map loadTradesFromFile() {
+        def file = new File("grails-app/services/io/xh/toolbox/data/CompanyTrades.json")
+        def input = (new JsonSlurper()).parseText(file.text)
+        input.each { it ->
+            it.profit_loss = Math.round(it.profit_loss * Math.random())
+            it.trade_volume = it.trade_volume * 1000000
+            it.active = it.trade_volume.toBigInteger() % 6 == 0
+        }
+        return [
+                trades : input,
+                summary: [
+                        id          : "summary",
+                        profit_loss : input.sum { it.profit_loss },
+                        trade_volume: input.sum { it.trade_volume }
+                ]
+        ]
+    }
+
+    void clearCaches() {
+        super.clearCaches()
+        trades = loadTradesFromFile()
+    }
+}
+
