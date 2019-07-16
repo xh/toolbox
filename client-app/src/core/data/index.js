229ca738c27bd5dd6a98413d5c79e43291d58583
inewell <inewell@andover.edu>
2019-07-16 10:58:35 -0400

    Remove SalesFigures.json import/export

--- client-app/src/core/data/index.js
+++ client-app/src/core/data/index.js
@@ -2,7 +2,6 @@ import companies from './CompanyRegistry';
 import companyTrades from './CompanyTrades';
 import {movies} from './Movies';
 import {restaurants} from './Restaurants';
-import salesFigures from './SalesFigures';
 import usStates from './USStates';
 
-export {companies, companyTrades, salesFigures, movies, usStates, restaurants};
+export {companies, companyTrades, movies, usStates, restaurants};
