ccf2d9b4d1a756a400ea018c45c52d7c57d20451
inewell <inewell@andover.edu>
2019-07-23 11:34:55 -0400

    WIP

--- grails-app/services/io/xh/toolbox/portfolio/PortfolioService.groovy
+++ grails-app/services/io/xh/toolbox/portfolio/PortfolioService.groovy
@@ -3,12 +3,10 @@ package io.xh.toolbox.portfolio
 import io.xh.hoist.BaseService
 
 import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.LocalTime
-import java.util.concurrent.ConcurrentHashMap
 
 import static io.xh.toolbox.portfolio.Utils.*
+import static java.time.ZoneOffset.UTC
 
 class PortfolioService extends BaseService {
 
@@ -20,11 +18,11 @@ class PortfolioService extends BaseService {
     private final int ORDERS_COUNT = 20000
 
     private List<Order> orders
-    private List<Position> rawPositions
+    private List<RawPosition> rawPositions
 
     void init() {
         orders = generateOrders()
-        rawPositions = calculateRawPositions()
+        rawPositions = calculateRawPositions(orders)
 
         super.init()
     }
@@ -33,34 +31,35 @@ class PortfolioService extends BaseService {
      * Return a portfolio of hierarchically grouped positions for the selected dimension(s).
      * @param dims - field names for dimensions on which to group.
      */
-    List<GroupedPosition> getPortfolio(List<String> dims, boolean includeSummary = false) {
+    List<Position> getPortfolio(List<String> dims, boolean includeSummary = false) {
 
-        List<GroupedPosition> positions = getPositions(dims)
+        List<Position> positions = getPositions(dims)
 
         return (!includeSummary) ? positions : [
-                new GroupedPosition([
+                new Position(
                         id      : 'summary',
                         name    : 'Total',
                         pnl     : positions.sum { it.pnl },
                         mktVal  : positions.sum { it.mktVal },
                         children: positions
-                ])
+                )
         ]
     }
 
 
     /**
-     * Return a single grouped position, uniquely identified by drilldown ID.
+     * Return a single grouped position, uniquely identified by drill-down ID.
      * @param positionId - ID installed on each position returned by `getPortfolio()`.
      */
-    GroupedPosition getPosition(String positionId) {
+    Position getPosition(String positionId) {
 
         Map parsedId = parsePositionId(positionId)
-        List dims = parsedId.keySet()
+        log.debug("getting position: " + positionId)
+        List dims = parsedId.keySet() as List
         List dimVals = parsedId.values()
 
-        List<GroupedPosition> positions = getPositions(dims)
-        GroupedPosition ret = null
+        List<Position> positions = getPositions(dims)
+        Position ret = null
 
         dimVals.each { dimVal ->
             ret = positions.find { it.name == dimVal }
@@ -75,12 +74,13 @@ class PortfolioService extends BaseService {
         if (!positionId)
             return []
         else {
-            Map posDimsMap = parsePositionId(positionId)
-            List dims = posDimsMap.keySet()
+            log.debug("getting position for order: " + positionId)
+            Map parseId = parsePositionId(positionId)
+            List dims = parseId.keySet() as List
+
             return orders.findAll { order ->
-                Map orderAsMap = order.formatForJSON()
-                return dims.every { dim ->
-                    posDimsMap[dim] == orderAsMap.position.formatForJSON()[dim]
+                dims.every { dim ->
+                    parseId[dim] == order."$dim"
                 }
             }
         }
@@ -92,7 +92,7 @@ class PortfolioService extends BaseService {
     // Implementation
     //------------------------
     private List<Order> generateOrders() {
-        List<Order> ret = new ArrayList(ORDERS_COUNT)
+        List<Order> ret = new ArrayList<Order>(ORDERS_COUNT)
         List<String> symbols = marketService.getAllSymbols() as List
         ORDERS_COUNT.times {i ->
 
@@ -107,12 +107,12 @@ class PortfolioService extends BaseService {
 
             // Calc 2nd order, partially random attributes
             MarketPrice mktData = sample(marketService.getMarketData(symbol))
-            Instant time = mktData.day.atTime(LocalTime.of(hour, min))
+            Instant time = mktData.day.atTime(LocalTime.of(hour, min)).toInstant(UTC)
             long quantity = randInt(300, 10000) * (dir == 'Sell' ? -1 : 1)
             double price = randDouble(mktData.low, mktData.high).round(2)
             long mktVal = (quantity * price).round()
 
-            orders << new Order(
+            ret << new Order(
                     id: "order-${i}",
                     symbol: symbol,
                     dir: dir,
@@ -128,58 +128,56 @@ class PortfolioService extends BaseService {
             )
         }
 
-        return orders.sort {it.time}
+        return ret.sort { it.time }
     }
 
     // Calculate lowest-level leaf positions with P&L.
-    private List<Position> calculateRawPositions() {
-        Map<String, List<Order>> byPosId = orders.groupBy { it.posId }
-        List<Position> positions = []
+    private List<RawPosition> calculateRawPositions(List<Order> orders) {
+        Map<String, List<Order>> bySymbol = orders.groupBy { it.symbol }
+        List<RawPosition> ret = []
 
-        byPosId.each { id, ordersForId ->
-            Order first = ordersForId.first()
-            String symbol = first.getSymbol()
-            double endPx = getMktData(symbol).last().getClose()
+        bySymbol.each { symbol, ordersForSymbol ->
+            Order first = ordersForSymbol.first()
+            double endPx = marketService.getMarketData(symbol).last().close
 
             def endQty = 0
             def netCashflow = 0
 
-            ordersForId = ordersForId.sort { it.time }
-            ordersForId.each {
+            ordersForSymbol = ordersForSymbol.sort { it.time }
+            ordersForSymbol.each {
                 endQty += it.quantity
                 netCashflow -= it.mktVal
             }
 
             // Crude P&L calc.
-            double endMktVal = endQty * endPx
-            double pnl = netCashflow + endMktVal
+            long endMktVal = (endQty * endPx).round()
+            long pnl = netCashflow + endMktVal
 
-            positions << new Position([
+            ret << new RawPosition(
                     symbol: symbol,
-                    sector: first.sector,
                     model : first.model,
                     fund  : first.fund,
-                    region: first.region,
                     trader: first.trader,
                     mktVal: endMktVal,
                     pnl   : pnl
-            ])
+            )
         }
 
-        return positions
+        return ret
     }
 
-    // Generate grouped, hierarchical position rollups for a list of one or more dimensions.
-    private List<GroupedPosition> getPositions(List<String> dims, List<Position> positions = rawPositions, String id = "root") {
+    // Generate grouped, hierarchical position roll-ups for a list of one or more dimensions.
+    private List<Position> getPositions(List<String> dims, List<RawPosition> positions = rawPositions, String id = 'root') {
         List<String> dimsCopy = dims.collect()  // Avoid mutating our input array.
 
         String dim = dimsCopy.first()
+        dimsCopy.remove(0)
         Map byDimVal = positions.groupBy { it[dim] }
-        List<GroupedPosition> ret = []
+        List<Position> ret = []
 
         byDimVal.each { dimVal, members ->
-            GroupedPosition groupPos = new GroupedPosition([
-                    // Generate a drilldown ID that encodes the path to this row.
+            Position pos = new Position([
+                    // Generate a drill-down ID that encodes the path to this row.
                     id    : id + ">>${dim}:${dimVal}",
                     name  : dimVal,
                     pnl   : members.sum { it.pnl },
@@ -188,23 +186,23 @@ class PortfolioService extends BaseService {
 
             // Recurse to create children for this node if additional dimensions remain.
             if (dimsCopy.size()) {
-                groupPos.setChildren(getPositions(dimsCopy, members, groupPos.id))
+                pos.setChildren(getPositions(dimsCopy, members, pos.id))
             }
 
-            ret << groupPos
+            ret << pos
         }
 
         return ret
     }
 
-    // Parse a drilldown ID from a rolled-up position into a map of all
+    // Parse a drill-down ID from a rolled-up position into a map of all
     // dimensions -> dim values contained within the rollup.
     private Map parsePositionId(String id) {
-        List<String> dims = id.split(">>").drop(1)
+        List<String> dims = id.split('>>').drop(1)
         Map posMap = [:]
 
         dims.each { dimStr ->
-            List<String> dimParts = dimStr.split(":")
+            List<String> dimParts = dimStr.split(':')
             posMap[dimParts[0]] = dimParts[1]
         }
 
