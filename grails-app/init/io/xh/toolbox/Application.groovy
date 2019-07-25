ab2de5b9e480d972eccca078e021f272de876a82
Anselm McClain <atm@xh.io>
2019-07-25 00:45:46 -0400

    App config boilerplate for websocket support

--- grails-app/init/io/xh/toolbox/Application.groovy
+++ grails-app/init/io/xh/toolbox/Application.groovy
@@ -2,9 +2,27 @@ package io.xh.toolbox
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import io.xh.hoist.websocket.HoistWebSockerHandler
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.handler.PerConnectionWebSocketHandler
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor
 
-class Application extends GrailsAutoConfiguration {
+@Configuration
+@EnableWebSocket
+class Application extends GrailsAutoConfiguration implements WebSocketConfigurer {
+
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+    }
+
+    @Override
+    void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        def handler = new PerConnectionWebSocketHandler(HoistWebSockerHandler.class)
+        registry.addHandler(handler, '/xhWebSocket')
+            .addInterceptors(new HttpSessionHandshakeInterceptor())
+            .setAllowedOrigins('*')
     }
 }
