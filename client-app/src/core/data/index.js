7a3b510b3507f7fa220c40aed1bd801722aa024c
inewell <inewell@andover.edu>
2019-07-25 17:16:31 -0400

    Take out unused files, standardize formats, and move companyTrades to server

--- client-app/src/core/data/index.js
+++ client-app/src/core/data/index.js
@@ -1,7 +1,5 @@
-import companies from './CompanyRegistry';
-import companyTrades from './CompanyTrades';
 import {movies} from './Movies';
 import {restaurants} from './Restaurants';
-import usStates from './USStates';
+import {usStates} from './USStates';
 
-export {companies, companyTrades, movies, usStates, restaurants};
+export {movies, usStates, restaurants};
