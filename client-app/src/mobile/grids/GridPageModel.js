018c8077df974e6fd36e7e5d1ee8f2c418bd0b51
inewell <inewell@andover.edu>
2019-07-25 17:15:59 -0400

    Get companyTrades from server

--- client-app/src/mobile/grids/GridPageModel.js
+++ client-app/src/mobile/grids/GridPageModel.js
@@ -1,10 +1,7 @@
-import {managed, HoistModel, LoadSupport} from '@xh/hoist/core';
-import {GridModel} from '@xh/hoist/cmp/grid';
+import {HoistModel, LoadSupport, managed, XH} from '@xh/hoist/core';
+import {emptyFlexCol, GridModel} from '@xh/hoist/cmp/grid';
 import {numberRenderer, thousandsRenderer} from '@xh/hoist/format';
-import {emptyFlexCol} from '@xh/hoist/cmp/grid';
 import {wait} from '@xh/hoist/promise';
-
-import {companyTrades} from '../../core/data';
 
 @HoistModel
 @LoadSupport
@@ -47,6 +44,7 @@ export class GridPageModel {
 
     async doLoadAsync(loadSpec) {
         await wait(500);
+        const companyTrades = await XH.fetchJson({url: 'customer'});
         this.gridModel.loadData(companyTrades);
     }
 
