24938f9a2d8da5a33b3f695cccda1626f7645f3d
inewell <inewell@andover.edu>
2019-07-25 15:58:42 -0400

    Remove unwanted extra content used for testing

--- client-app/src/desktop/tabs/panels/ToolbarPanelModel.js
+++ client-app/src/desktop/tabs/panels/ToolbarPanelModel.js
@@ -6,13 +6,4 @@ export class ToolbarPanelModel {
     @bindable state = null;
     @bindable enableTerminate = false;
     @bindable visible = false;
-
-
-    constructor() {
-        this.addReaction({
-            track: () => this.visible,
-            run: () => console.log('Hello There')
-        });
-    }
-
 }
