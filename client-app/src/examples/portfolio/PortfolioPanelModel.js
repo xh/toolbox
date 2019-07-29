d591fb27d7fabca6dc155dfbdcf30f95fa17d3ca
inewell <inewell@andover.edu>
2019-07-29 12:13:40 -0400

    Rework to include treemap. Still need to resolve unmountOnHide problem.

--- client-app/src/examples/portfolio/PortfolioPanelModel.js
+++ client-app/src/examples/portfolio/PortfolioPanelModel.js
@@ -4,34 +4,51 @@ import {OrdersPanelModel} from './OrdersPanelModel';
 import {LineChartModel} from './LineChartModel';
 import {OHLCChartModel} from './OHLCChartModel';
 import {bindable} from '@xh/hoist/mobx';
+import {SplitTreeMapModel} from '@xh/hoist/desktop/cmp/treemap';
 
 @HoistModel
 @LoadSupport
 export class PortfolioPanelModel {
 
     @managed positionsPanelModel = new PositionsPanelModel();
+    @managed splitTreeMapModel = new SplitTreeMapModel({
+        gridModel: this.positionsPanelModel.gridModel,
+        filter: (rec) => {
+            return rec.pnl >= 0;
+        },
+        treeMapModelConfig: {
+            labelField: 'name',
+            valueField: 'pnl',
+            heatField: 'pnl',
+            valueFieldLabel: 'Pnl'
+        },
+        orientation: 'horizontal'
+    });
     @managed ordersPanelModel = new OrdersPanelModel();
     @managed lineChartModel = new LineChartModel();
     @managed ohlcChartModel = new OHLCChartModel();
 
     @bindable displayedOrderSymbol = '';
+    @bindable selectedOrder = null;
 
     get selectedPosition() {
         return this.positionsPanelModel.selectedRecord;
     }
 
-    get selectedOrder() {
-        return this.ordersPanelModel.selectedRecord;
-    }
+    // get selectedOrder() {
+    //     return this.ordersPanelModel.selectedRecord;
+    // }
 
     constructor() {
         this.addReaction(this.selectedPositionReaction());
         this.addReaction(this.selectedOrderReaction());
+        this.setSelectedOrder(this.ordersPanelModel.selectedRecord);
     }
 
     async doLoadAsync(loadSpec) {
         await loadAllAsync([
             this.positionsPanelModel,
+            // this.splitTreeMapModel,
             this.ordersPanelModel,
             this.lineChartModel,
             this.ohlcChartModel
@@ -53,12 +70,17 @@ export class PortfolioPanelModel {
 
     selectedOrderReaction() {
         return {
-            track: () => this.selectedOrder,
+            track: () => this.ordersPanelModel.selectedRecord,
             run: (order) => {
-                const symbol = order ? order.symbol : null;
-                this.setDisplayedOrderSymbol(symbol || '');
-                this.lineChartModel.setOrderSymbol(symbol);
-                this.ohlcChartModel.setOrderSymbol(symbol);
+                const orderStr = order ? order.symbol : 'null';
+                console.log('ORDER: '+orderStr);
+                if (order) {
+                    this.setSelectedOrder(order);
+                    const symbol = order.symbol;
+                    this.setDisplayedOrderSymbol(symbol || '');
+                    this.lineChartModel.setOrderSymbol(symbol);
+                    this.ohlcChartModel.setOrderSymbol(symbol);
+                }
             },
             delay: 500
         };
