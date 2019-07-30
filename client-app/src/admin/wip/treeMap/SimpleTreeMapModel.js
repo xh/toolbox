9b4383c156d555a7ed6ca81285f4b616591eaed0
Tom Tirapani <hello@tomtirapani.com>
2019-07-30 14:34:46 +0100

    TreeMap: Require either Store or GridModel - can't set data directly

--- client-app/src/admin/wip/treeMap/SimpleTreeMapModel.js
+++ client-app/src/admin/wip/treeMap/SimpleTreeMapModel.js
@@ -1,13 +1,20 @@
 import {XH, HoistModel, managed} from '@xh/hoist/core';
 import {LoadSupport} from '@xh/hoist/core/mixins';
 import {TreeMapModel} from '@xh/hoist/desktop/cmp/treemap';
+import {Store} from '@xh/hoist/data';
 
 @HoistModel
 @LoadSupport
 export class SimpleTreeMapModel {
 
     @managed
+    store = new Store({
+        fields: ['name', 'pnl', 'mktVal']
+    });
+
+    @managed
     treeMapModel = new TreeMapModel({
+        store: this.store,
         labelField: 'name',
         valueField: 'pnl',
         heatField: 'mktVal',
@@ -17,7 +24,7 @@ export class SimpleTreeMapModel {
 
     async doLoadAsync() {
         const data = await XH.portfolioService.getPortfolioAsync(['symbol']);
-        this.treeMapModel.setData(data);
+        this.store.loadData(data);
     }
 
 }
