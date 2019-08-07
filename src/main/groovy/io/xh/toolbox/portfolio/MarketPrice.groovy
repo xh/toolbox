f3c845fa8372a55998666d6f50e4c059651f7ca1
inewell <inewell@andover.edu>
2019-08-07 12:41:23 -0400

    Add perturb method

--- src/main/groovy/io/xh/toolbox/portfolio/MarketPrice.groovy
+++ src/main/groovy/io/xh/toolbox/portfolio/MarketPrice.groovy
@@ -3,6 +3,7 @@ package io.xh.toolbox.portfolio
 import io.xh.hoist.json.JSONFormatCached
 
 import java.time.LocalDate
+import static Utils.randDouble
 
 class MarketPrice extends JSONFormatCached {
     LocalDate day
@@ -11,6 +12,24 @@ class MarketPrice extends JSONFormatCached {
     double open
     double close
     long volume
+
+
+    MarketPrice perturb(Float pctRange) {
+        double newClose = close * (1+(randDouble(-pctRange, pctRange)/100))
+        double newHigh = newClose > high ? newClose : high
+        double newLow = newClose < low ? newClose : low
+
+        return new MarketPrice(
+                day: day,
+                open: open,
+                volume: volume,
+                high: newHigh,
+                low: newLow,
+                close: newClose
+        )
+    }
+
+
 
     Map formatForJSON() {
         return [
