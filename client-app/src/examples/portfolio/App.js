b5bac4be58255f319c7851fc9413c94ba864b99f
inewell <inewell@andover.edu>
2019-08-09 17:13:39 -0400

    Add websocket indicator

--- client-app/src/examples/portfolio/App.js
+++ client-app/src/examples/portfolio/App.js
@@ -2,9 +2,10 @@ import {Component} from 'react';
 import {HoistComponent} from '@xh/hoist/core';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {Icon} from '@xh/hoist/icon';
-import {appBar} from '@xh/hoist/desktop/cmp/appbar';
+import {appBar, appBarSeparator} from '@xh/hoist/desktop/cmp/appbar';
 import {ContextMenuSupport, ContextMenuItem} from '@xh/hoist/desktop/cmp/contextmenu';
 import {portfolioPanel} from './PortfolioPanel';
+import {webSocketIndicator} from '@xh/hoist/cmp/websocket';
 
 @HoistComponent
 @ContextMenuSupport
@@ -15,6 +16,10 @@ export class App extends Component {
             tbar: appBar({
                 icon: Icon.portfolio({size: '2x', prefix: 'fal'}),
                 title: 'Portfolio',
+                rightItems: [
+                    webSocketIndicator({iconOnly: true, marginRight: 4}),
+                    appBarSeparator()
+                ],
                 hideRefreshButton: false
             }),
             item: portfolioPanel()
