4bdfa2bc2b8c11fc3edd990e5be5b31ecac53f86
inewell <inewell@andover.edu>
2019-08-09 12:53:29 -0400

    Push diff to client

--- src/main/groovy/io/xh/toolbox/portfolio/PositionSession.groovy
+++ src/main/groovy/io/xh/toolbox/portfolio/PositionSession.groovy
@@ -21,13 +21,45 @@ class PositionSession implements JSONFormat {
     }
 
     void pushUpdate() {
-        Utils.webSocketService.pushToChannel(channelKey, topic, 'hola')
+        PositionResultSet newPositions = positionService.getPositions(positions.query)
+
+        // find changed positions, and send them as a flat list of updates.
+        Map<String, Position> oldPositionMap = getMappedPositions(positions.root)
+        Map<String, Position> newPositionMap = getMappedPositions(newPositions.root)
+
+        def allIds = oldPositionMap.keySet()
+        List<Position> changedPositions = allIds.findAll { id ->
+            if (id == 'root') return false
+            Position oldPos = oldPositionMap[id]
+            Position newPos = newPositionMap[id]
+            // Position tree structure is the same in new positions and old, as only prices have changed.
+            // So, positions are different iff their pnl and/or mktVal values differ.
+            return (newPos.pnl != oldPos.pnl || newPos.mktVal != oldPos.mktVal)
+        }.collect { id -> newPositionMap[id] }
+
+        Utils.webSocketService.pushToChannel(channelKey, topic, changedPositions)
     }
 
 
     //--------------------
     // Implementation
     //--------------------
+
+    private Map<String, Position> getMappedPositions(Position root) {
+        Map<String, Position> ret = [:]
+        Closure<Void> addToPositionMapRecursive
+        addToPositionMapRecursive = { Position pos ->
+            ret[pos.id] = pos
+            if (pos.children) {
+                pos.children.each { childPos ->
+                    addToPositionMapRecursive(childPos)
+                }
+            }
+            return
+        }
+        addToPositionMapRecursive(root)
+        return ret
+    }
 
     private PositionService getPositionService() {
         Utils.appContext.positionService
@@ -41,6 +73,7 @@ class PositionSession implements JSONFormat {
         return [
                 id: id,
                 channelKey: channelKey,
+                topic: topic,
                 positions: positions
         ]
     }
