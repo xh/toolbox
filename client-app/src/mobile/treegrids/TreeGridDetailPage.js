ce793b858774cfcc8fc12c748042b4a37208b587
lbwexler <lbwexler@xh.io>
2019-10-02 19:24:52 -0400

    Last Mobile Functional Components for Toolbox (outside of inputs)

--- client-app/src/mobile/treegrids/TreeGridDetailPage.js
+++ client-app/src/mobile/treegrids/TreeGridDetailPage.js
@@ -1,58 +1,72 @@
-import {Component} from 'react';
-import {HoistComponent, elemFactory} from '@xh/hoist/core';
+import {hoistCmp, HoistModel, LoadSupport, useLocalModel, XH} from '@xh/hoist/core';
 import {div} from '@xh/hoist/cmp/layout';
 import {page} from '@xh/hoist/mobile/cmp/page';
 import {numberRenderer} from '@xh/hoist/format';
 import {capitalize} from 'lodash';
 import {Icon} from '@xh/hoist/icon';
+import {bindable} from '@xh/hoist/mobx';
 
-import {TreeGridDetailPageModel} from './TreeGridDetailPageModel';
-
-@HoistComponent
-export class TreeGridDetailPage extends Component {
-
-    model = new TreeGridDetailPageModel({id: this.props.id});
-    
-    render() {
-        const {record, loadModel} = this.model;
-
+export const TreeGridDetailPage = hoistCmp({
+    render({id}) {
+        const impl = useLocalModel(LocalModel);
+        impl.setId(decodeURIComponent(id));
+        const {record} = impl;
+        
         return page({
-            title: record ? this.renderPageTitle(record) : null,
+            title: record ? renderPageTitle(record) : null,
             icon: Icon.portfolio(),
-            mask: loadModel,
+            mask: impl.loadModel,
             className: 'toolbox-detail-page',
-            item: record ? this.renderRecord(record) : null
+            item: record ? renderRecord(record) : null
         });
     }
+});
 
-    renderPageTitle(record) {
-        const lastPart = record.id.split('>>').pop();
-        return lastPart.split(':').pop();
-    }
-
-    renderRecord(record) {
-        return div(
-            // Split id to extract drilldown information
-            ...record.id.split('>>').map(it => {
-                if (it === 'root') return null;
-                const parts = it.split(':');
-                return this.renderRow(capitalize(parts[0]), parts[1]);
-            }),
-
-            this.renderRow('Market Value', record.mktVal, numberRenderer({precision: 0, ledger: true, asElement: true})),
-            this.renderRow('P&L', record.pnl, numberRenderer({precision: 0, ledger: true, colorSpec: true, asElement: true}))
-        );
-    }
-
-    renderRow(title, value, renderer) {
-        return div({
-            className: 'toolbox-detail-row',
-            items: [
-                div(title),
-                div(renderer ? renderer(value) : value)
-            ]
-        });
-    }
+function renderPageTitle(record) {
+    const lastPart = record.id.split('>>').pop();
+    return lastPart.split(':').pop();
 }
 
-export const treeGridDetailPage = elemFactory(TreeGridDetailPage);
+function renderRecord(record) {
+    return div(
+        // Split id to extract drilldown information
+        ...record.id.split('>>').map(it => {
+            if (it === 'root') return null;
+            const parts = it.split(':');
+            return renderRow(capitalize(parts[0]), parts[1]);
+        }),
+
+        renderRow('Market Value', record.mktVal, numberRenderer({precision: 0, ledger: true, asElement: true})),
+        renderRow('P&L', record.pnl, numberRenderer({precision: 0, ledger: true, colorSpec: true, asElement: true}))
+    );
+}
+
+function renderRow(title, value, renderer) {
+    return div({
+        className: 'toolbox-detail-row',
+        items: [
+            div(title),
+            div(renderer ? renderer(value) : value)
+        ]
+    });
+}
+
+@LoadSupport
+@HoistModel
+class LocalModel {
+
+    @bindable id;
+    @bindable.ref record;
+
+    constructor() {
+        this.addReaction({
+            track: () => this.id,
+            run: () => this.loadAsync()
+        });
+    }
+
+    async doLoadAsync(loadSpec) {
+        const record = await (this.id ? XH.portfolioService.getPositionAsync(this.id) : null);
+        this.setRecord(record);
+    }
+}
