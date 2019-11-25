26567187d28197b26576860eb346d69a4f6711ef
Peter Coffman <petercoffman@Peters-MBP.home>
2019-11-25 16:26:55 -0500

    Remove unused import

--- grails-app/init/io/xh/toolbox/BootStrap.groovy
+++ grails-app/init/io/xh/toolbox/BootStrap.groovy
@@ -1,7 +1,6 @@
 package io.xh.toolbox
 
 import io.xh.hoist.config.AppConfig
-import io.xh.hoist.config.ConfigService
 import io.xh.hoist.util.Utils
 import io.xh.toolbox.user.User
 import io.xh.hoist.BaseService
