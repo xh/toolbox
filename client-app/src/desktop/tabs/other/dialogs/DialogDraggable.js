a0f4716f2ed4bee3627c314fa369f825364c0d67
Colin Rudd <cnrudd@gmail.com>
2020-01-03 18:24:05 -0500

    Use same form in fixed and in draggable demo dialogs.

--- client-app/src/desktop/tabs/other/dialogs/DialogDraggable.js
+++ client-app/src/desktop/tabs/other/dialogs/DialogDraggable.js
@@ -1,50 +1,26 @@
 // import {FocusStyleManager} from '@blueprintjs/core';
 import {HoistModel, hoistCmp} from '@xh/hoist/core';
 import {bindable} from '@xh/hoist/mobx';
-
-
-import {filler} from '@xh/hoist/cmp/layout';
-import {panel} from '@xh/hoist/desktop/cmp/panel';
-import {button} from '@xh/hoist/desktop/cmp/button';
-import {Icon} from '@xh/hoist/icon';
-
 import {dialog} from '@xh/hoist/desktop/cmp/dialog';
+
+import {formPanel} from './forms/FormPanel';
 
 
 export const dialogDraggable = hoistCmp.factory({
 
     render({model}) {
-
         const onCloseClick = () => {
-                model.setIsOpen(false);
-            },
-            width = 600,
-            height = 200;
+            model.setIsOpen(false);
+        };
 
         return dialog({
             isOpen: model.isOpen,
             close: onCloseClick,
             canEscapeKeyClose: true,
             isDraggable: true,
-            width,
-            height,
-            item: panel({
-                title: 'Fixed Dialog',
-                icon: Icon.lock(),
-                width,
-                height,
-                headerItems: [button({
-                    icon: Icon.close(),
-                    onClick: onCloseClick
-                })],
-                bbar: [
-                    filler(),
-                    button({
-                        text: 'Ok',
-                        onClick: onCloseClick
-                    })
-                ]
-            })
+            width: 870,
+            height: 550,
+            item: formPanel({onCloseClick})
         });
     }
 });
