5ca816296e17972f7583023efff2d8817eab8be5
Colin Rudd <cnrudd@gmail.com>
2020-01-07 14:21:27 -0500

    Cleanup model conf

--- client-app/src/desktop/tabs/other/dialogs/DialogModels.js
+++ client-app/src/desktop/tabs/other/dialogs/DialogModels.js
@@ -2,15 +2,13 @@ import { DialogModel } from '@xh/hoist/desktop/cmp/dialog';
 
 
 export const dialogDraggableModel = new DialogModel({
-    canEscapeKeyClose: true,
+    resizable: true,
     draggable: true,
     width: 870,
     height: 550
 });
 
 export const dialogNotDraggableModel = new DialogModel({
-    canEscapeKeyClose: true,
-    draggable: false,
     width: 870,
     height: 550
 });
