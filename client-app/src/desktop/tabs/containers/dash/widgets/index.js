adf1a3abd58a1444213a4958efd638de547b5cab
Lee Wexler <lbwexler@xh.io>
2020-01-16 10:52:51 -0500

    Dash refactor (#321)

--- client-app/src/desktop/tabs/containers/dash/widgets/index.js
+++ client-app/src/desktop/tabs/containers/dash/widgets/index.js
@@ -1,7 +1,5 @@
 export * from './ButtonWidget';
-export * from './ButtonWidgetModel';
 export * from './ChartWidget';
 export * from './GridWidget';
-export * from './GridWidgetModel';
 export * from './PanelWidget';
 export * from './TreeGridWidget';
