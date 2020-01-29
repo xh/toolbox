b2be189f43763f80ab2c3e8780b6d78a4d8239c8
lbwexler <lbwexler@xh.io>
2020-01-29 12:44:45 -0500

    New controller
    Lee and Petra co-dev

---
+++ grails-app/controllers/io/xh/toolbox/admin/ProjectRestController.groovy
@@ -0,0 +1,26 @@
+/*
+ * This file belongs to Hoist, an application development toolkit
+ * developed by Extremely Heavy Industries (www.xh.io | info@xh.io)
+ *
+ * Copyright © 2020 Extremely Heavy Industries Inc.
+ */
+
+package io.xh.toolbox.admin
+
+import io.xh.hoist.RestController
+import io.xh.hoist.security.Access
+import io.xh.toolbox.roadmap.Project
+import org.grails.web.json.JSONObject
+
+@Access(['HOIST_ADMIN'])
+class ProjectRestController extends RestController {
+
+    static restTarget = Project
+    static trackChanges = true
+
+    def lookupData() {
+        renderJSON (
+                statuses: Project.STATUSES
+        )
+    }
+}
