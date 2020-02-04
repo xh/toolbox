76369efab79e28c8115fc4d7bbe5348ed8860578
lbwexler <lbwexler@xh.io>
2020-02-04 14:26:27 -0500

    Enhance example style

--- client-app/src/mobile/pinPad/PinPadPage.js
+++ client-app/src/mobile/pinPad/PinPadPage.js
@@ -1,84 +1,85 @@
-import {hoistCmp, HoistModel, useLocalModel} from '@xh/hoist/core';
+import {hoistCmp, HoistModel, creates, managed} from '@xh/hoist/core';
 import {page} from '@xh/hoist/mobile/cmp/page';
 import {pinPad} from '@xh/hoist/mobile/cmp/pinpad/PinPad';
 import {PinPadModel} from '@xh/hoist/mobile/cmp/pinpad/PinPadModel';
 import {bindable} from '@xh/hoist/mobx';
 import {p} from '@xh/hoist/cmp/layout';
 import {panel} from '@xh/hoist/mobile/cmp/panel';
+import {wait} from '@xh/hoist/promise';
 
 import './PinPadPage.scss';
 
 export const pinPadPage = hoistCmp.factory({
-    render() {
-        const model = useLocalModel(LocalModel);
-        return page(
-            model.loggedIn ?
-                panel({
-                    className: 'toolbox-page pinpad-page-secrets',
-                    title: 'Secret plans',
-                    items: [
-                        p('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porta velit varius augue fermentum, vulputate tempus magna tempus.'),
-                        p('Fusce consectetur malesuada vehicula. Aliquam commodo magna at porta sollicitudin. Sed laoreet vehicula leo vel aliquam. Aliquam auctor fringilla ex, nec iaculis felis tincidunt ac. Pellentesque blandit ipsum odio, vel lacinia arcu blandit non.'),
-                        p('Vestibulum non libero sem. Mauris a ipsum elit. Donec vestibulum sodales dapibus. Mauris posuere facilisis mollis. Etiam nec mauris nunc. Praesent mauris libero, blandit gravida ullamcorper vel, condimentum et velit. Suspendisse fermentum odio ac dui aliquet semper. Duis arcu felis, accumsan in leo sit amet, vehicula imperdiet tellus. Nulla ut condimentum quam. Donec eget mauris vitae libero blandit facilisis efficitur id justo.'),
-                        p('Nam et tincidunt risus, at faucibus enim. Aliquam tortor est, finibus ac metus id, eleifend auctor quam. Aenean purus odio, tempus interdum velit et, faucibus placerat nisi. Etiam eget nunc vehicula, eleifend justo quis, varius leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris bibendum mollis tempor.'),
-                        p('Fusce ac sollicitudin nunc, at tempus sem. Fusce dapibus lorem malesuada vestibulum luctus. Etiam semper est in ligula sagittis facilisis. Phasellus accumsan placerat ex, eu fringilla mauris semper nec.')
-                    ]
-                }) :
-                pinPad({model: model.model})
-        );
+
+    model: creates(() => new Model()),
+
+    render({model}) {
+        return page(!model.loggedIn ? pinPad() : secretPlans());
     }
 });
 
+const secretPlans = hoistCmp.factory(
+    () => panel({
+        className: 'toolbox-page pinpad-page-secrets',
+        title: 'Secret plans',
+        items: [
+            p('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porta velit varius augue fermentum, vulputate tempus magna tempus.'),
+            p('Fusce consectetur malesuada vehicula. Aliquam commodo magna at porta sollicitudin. Sed laoreet vehicula leo vel aliquam. Aliquam auctor fringilla ex, nec iaculis felis tincidunt ac. Pellentesque blandit ipsum odio, vel lacinia arcu blandit non.'),
+            p('Vestibulum non libero sem. Mauris a ipsum elit. Donec vestibulum sodales dapibus. Mauris posuere facilisis mollis. Etiam nec mauris nunc. Praesent mauris libero, blandit gravida ullamcorper vel, condimentum et velit. Suspendisse fermentum odio ac dui aliquet semper. Duis arcu felis, accumsan in leo sit amet, vehicula imperdiet tellus. Nulla ut condimentum quam. Donec eget mauris vitae libero blandit facilisis efficitur id justo.'),
+            p('Nam et tincidunt risus, at faucibus enim. Aliquam tortor est, finibus ac metus id, eleifend auctor quam. Aenean purus odio, tempus interdum velit et, faucibus placerat nisi. Etiam eget nunc vehicula, eleifend justo quis, varius leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris bibendum mollis tempor.'),
+            p('Fusce ac sollicitudin nunc, at tempus sem. Fusce dapibus lorem malesuada vestibulum luctus. Etiam semper est in ligula sagittis facilisis. Phasellus accumsan placerat ex, eu fringilla mauris semper nec.')
+        ]
+    })
+);
+
 @HoistModel
-class LocalModel {
-    model;
+class Model {
+
+    @managed
+    pinPadModel = new PinPadModel({
+        pinLength: 5,
+        headerText: 'Enter PIN...'
+    });
+
     attempts = 0;
     maxAttempts = 5;
-    @bindable loggedIn = false;
-    constructor() {
-        this.model = new PinPadModel({
-            pinLength: 5,
-            headerText: 'Enter PIN...'
-        });
 
+    @bindable loggedIn = false;
+
+    constructor() {
+        const {pinPadModel: pad} = this;
         this.addReaction({
-            track: () => this.model.completedPin,
+            track: () => pad.completedPin,
             run: (completedPin) => {
-                const {model} = this;
-                if (!completedPin) {
-                    return;
-                }
-                model.setSubHeaderText('Checking PIN...');
-                model.setDisabled(true);
-                setTimeout(() => {
-                    this.handleCompletedPin(completedPin);
-                }, 500);
+                if (!completedPin) return;
+
+                pad.setSubHeaderText('Checking PIN...');
+                pad.setDisabled(true);
+                wait(500).then(() => this.handleCompletedPin(completedPin));
             }
         });
     }
 
     handleCompletedPin(completedPin) {
-        const {model, attempts, maxAttempts} = this;
+        const {pinPadModel: pad, attempts, maxAttempts} = this;
         if (completedPin === '12345') {
-            model.setErrorText('');
-            model.setHeaderText('Access Granted.');
-            model.setSubHeaderText('Welcome to XH.io');
-            setTimeout(() => {
-                this.setLoggedIn(true);
-            }, 1000);
+            pad.setErrorText('');
+            pad.setHeaderText('Access Granted.');
+            pad.setSubHeaderText('Welcome to XH.io');
+            wait(1000).then(() => this.setLoggedIn(true));
         } else if (attempts >= maxAttempts) {
-            model.setHeaderText('Account Locked.');
-            model.setSubHeaderText('Login disabled at this time.');
-            model.setErrorText('You have made too many attempts to log in. Contact support for help.');
+            pad.setHeaderText('Account Locked.');
+            pad.setSubHeaderText('Login disabled at this time.');
+            pad.setErrorText('You have made too many attempts to log in. Contact support for help.');
         } else {
-            model.setErrorText(`PIN not recognized. Try again. Account will be locked after ${5 - attempts} attempts.`);
-            model.setSubHeaderText(
+            pad.setErrorText(`PIN not recognized. Try again. Account will be locked after ${5 - attempts} attempts.`);
+            pad.setSubHeaderText(
                 attempts === maxAttempts - 2 ? 'Access Denied. (Use the Schwartz!)' :
                     attempts === maxAttempts - 1 ? 'Access Denied. (Try \'12345\')' :
                         'Access Denied'
             );
-            model.setDisabled(false);
-            model.clear();
+            pad.setDisabled(false);
+            pad.clear();
 
             this.attempts++;
         }
