1c9086bfe1307cddd9e5976370b437ff14466fda
Petra Laohakul <petra@xh.io>
2020-02-26 16:59:08 -0500

    Checkpoint

--- client-app/src/desktop/tabs/other/ExceptionsPanel.js
+++ client-app/src/desktop/tabs/other/ExceptionsPanel.js
@@ -1,11 +1,28 @@
 import {wrapper} from '../../common';
 import {hoistCmp} from '@xh/hoist/core';
-import {p} from '@xh/hoist/cmp/layout';
+import {box, p} from '@xh/hoist/cmp/layout';
+import {button} from '@xh/hoist/desktop/cmp/button';
+import {panel} from '@xh/hoist/desktop/cmp/panel';
+import {Icon} from '@xh/hoist/icon/Icon';
+import './ExceptionsPanel.scss';
+import {XH} from '@xh/hoist/core';
 
 export const exceptionsPanel = hoistCmp.factory(
     () => wrapper({
-        description: [p('Exceptions')],
+        description: [p('Exception')],
         links: [],
-        item: p('Exception')
+        item: box({
+            className: 'tb-exceptions',
+            item: panel({
+                title: 'Exception Handling',
+                icon: Icon.warning(),
+
+                item: button({
+                    text: 'Click me to throw an error',
+                    icon: Icon.error(),
+                    onClick: () => XH.handleException('You threw an error!', {logOnServer: false})
+                })
+            })
+        })
     })
 );
