ad0c5a3db6c73782ae86b28bc363989ef98eda94
Petra Laohakul <petra@xh.io>
2020-03-03 12:23:00 -0500

    Used code elemfactory in descriptions

--- client-app/src/desktop/tabs/other/ExceptionsPanel.js
+++ client-app/src/desktop/tabs/other/ExceptionsPanel.js
@@ -1,6 +1,6 @@
 import {wrapper} from '../../common';
 import {hoistCmp, HoistModel} from '@xh/hoist/core';
-import {box, filler, hframe, p, vbox} from '@xh/hoist/cmp/layout';
+import {box, code, filler, hframe, p, vbox} from '@xh/hoist/cmp/layout';
 import {button} from '@xh/hoist/desktop/cmp/button';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {Icon} from '@xh/hoist/icon/Icon';
@@ -14,7 +14,15 @@ export const exceptionsPanel = hoistCmp.factory({
     model: creates(() => new Model()),
     render: ({model}) => wrapper({
         description: [
-            p('Hoist\'s Exception Handler standardizes and facilitates catching exceptions and errors on server and client sides. When an exception is caught, the Exception Dialog component is able to present the error in a stringified, human-readable format and offer more details, like the HTTP status code, error message, and stack trace.'),
+            p({
+                items: [
+                    'Hoist\'s ',
+                    code('ExceptionHandler'),
+                    ' standardizes and facilitates catching exceptions and errors on server and client sides. When an exception is caught, the ',
+                    code('ExceptionDialog'),
+                    ' component is able to present the error in a stringified, human-readable format and offer more details, like the HTTP status code, error message, and stack trace.'
+                ]
+            }),
             p('Some errors may require an app refresh, while others do not. Errors may also be marked as routine and need not reveal further details to the user beyond the stringified error message. Exception handling can also be customized with config options as displayed in the examples below.'),
             p('Users also have the option to send a message to their configured support email address (e.g. support@xh.io for Toolbox) to report additional information about the error.')
         ],
@@ -41,7 +49,13 @@ const exceptionPanel = hoistCmp.factory(
             title: 'Exception Handling',
             icon: Icon.warning(),
             items: [
-                p('In the `fooBar` function below, the Exception Handler catches the error and returns a readable error message in the Exception Dialog. Toggle the options below to customize exception handling on this Reference Error below.'),
+                p({
+                    items: [
+                        'In the ',
+                        code('fooBar'),
+                        ' function below, the Exception Handler catches the error and returns a readable error message in the Exception Dialog. Toggle the options below to customize exception handling on this Reference Error below.'
+                    ]
+                }),
                 codeInput({
                     width: 'fill',
                     height: 135,
@@ -104,7 +118,15 @@ const promisePanel = hoistCmp.factory(
             title: 'Promise Error Catching',
             icon: Icon.warning(),
             items: [
-                p('Hoist provides neat .catchDefault() and .catchDefaultWhen() methods that will handle errors thrown when promises are rejected. (No need to wrap in try/catch logic!)'),
+                p({
+                    items: [
+                        'Hoist provides useful ',
+                        code('.catchDefault()'),
+                        ' and ',
+                        code('.catchDefaultWhen()'),
+                        ' methods that will handle errors thrown when promises are rejected. (No need to wrap in try/catch logic!)'
+                    ]
+                }),
                 codeInput({
                     height: 150,
                     width: 'fill',
