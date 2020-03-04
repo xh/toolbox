52b6e320397f90d4ed2682f4a015b405f7836560
Petra Laohakul <petra@xh.io>
2020-03-04 17:15:25 -0500

    Major refactoring with Lee

--- client-app/src/desktop/tabs/other/PreferenceExamplePanel.js
+++ client-app/src/desktop/tabs/other/PreferenceExamplePanel.js
@@ -1,7 +1,7 @@
 import {wrapper} from '../../common';
 import {hoistCmp, HoistModel} from '@xh/hoist/core';
-import {box, code, h3, hbox, hframe, p, vframe} from '@xh/hoist/cmp/layout';
-import {button} from '@xh/hoist/desktop/cmp/button';
+import {code, h3, hbox, hframe, p, vbox, vframe} from '@xh/hoist/cmp/layout';
+// import {button} from '@xh/hoist/desktop/cmp/button';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {Icon} from '@xh/hoist/icon/Icon';
 import './PreferenceExamplePanel.scss';
@@ -15,154 +15,164 @@ export const preferenceExamplePanel = hoistCmp.factory({
     render: ({model}) => wrapper({
         description: [
             p({
-                items: ['Here in Toolbox, users are able to set local user app preferences in App Menu Options via ',
+                items: [
+                    'Hoist allows applications to easily define per-user preferences that can be saved in either ' +
+                    'local storage or the database. Apps can easily read and write values to this data service' +
+                    'via ',
                     code('PrefService'),
-                    '. Preferences are created by admin and can be extended to the user to set in Local Storage, where they persist until cleared.']
+                    ' and hoist will handle persisting all changes.'
+                ]
             }),
-            p('In the toy app below, you can experiment with setting preferences and refresh the browser to check that they stick. The Application tab in Chrome Dev Tools will also expose all preferences in Local Storage according to readable key-value pairs.')
+            p({
+                items: ['In the example below, you can experiment with setting preferences and ',
+                    'refresh the browser to check that they stick. Examine Local Storage in Chrome Dev Tools to see raw data in ',
+                    code('toolbox.[user]@xh.io.localPrefs')
+                ]
+            })
         ],
         links: [
             {url: '$HR/admin/tabs/preferences/PreferencePanel.js', notes: 'Preferences Panel'}
         ],
-        item: box({
+        item: vbox({
             className: 'tb-preferences',
             items: [
-                panel({
-                    title: 'User Preferences',
-                    className: model.background,
-                    icon: Icon.options(),
-                    item: hframe({
-                        className: 'user-profile',
-                        items: [
-                            vframe({
-                                className: 'user-profile--card',
-                                items: [
-                                    h3(`${XH.getUser().displayName}`),
-                                    model.icon
-                                ]
-                            }),
-                            vframe({
-                                className: model.className,
-                                items: [
-                                    p(`First Name: ${XH.getUser().firstName}`),
-                                    p(`Last Name: ${XH.getUser().lastName}`),
-                                    p(`Username: ${XH.getUser().username}`),
-                                    p(`Email: ${XH.getUser().email}`)
-                                ]
-                            })
-                        ]
-                    }),
-                    bbar: [
-                        panel({
-                            flex: 1,
-                            height: 240,
-                            title: 'PrefService at Work',
-                            icon: Icon.gears(),
-                            compactHeader: true,
-                            items: [
-                                hbox({
-                                    width: 'fill',
-                                    items: [
-                                        p('Set Text Color:'),
-                                        select({
-                                            bind: 'color',
-                                            options: [
-                                                {label: 'Red', value: 'red'},
-                                                {label: 'Blue', value: 'blue'},
-                                                {label: 'Green', value: 'green'}
-                                            ]
-                                        })
-                                    ]
-                                }),
-                                hbox(
-                                    p('Set User Icon:'),
-                                    select({
-                                        bind: 'userIcon',
-                                        options: [
-                                            {label: 'User', value: 'user'},
-                                            {label: 'Chess Knight', value: 'knight'},
-                                            {label: 'Sun', value: 'sun'}
-                                        ]
-                                    })
-                                ),
-                                hbox(
-                                    switchInput({
-                                        bind: 'showBackground',
-                                        label: 'Show Background Image'
-                                    })
-                                ),
-                                button({
-                                    icon: Icon.refresh(),
-                                    text: 'Save Preferences and Reload',
-                                    minimal: false,
-                                    onClick: () => model.saveAndReload()
-                                })
-                            ]
-                        }),
-                        panel({
-                            compactHeader: true,
-                            height: 240,
-                            title: 'Output',
-                            icon: Icon.code(),
-                            model: {side: 'right', defaultSize: 300},
-                            item: codeInput({
-                                height: 230,
-                                bind: 'codeOutput'
-                            })
-                        })
-                    ]
-                })
+                userProfile(),
+                hbox(
+                    preferenceEditor(),
+                    codeOutput()
+                )
             ]
         })
     })
 });
 
+const userProfile = hoistCmp.factory(
+    ({model}) => {
+        const user = XH.getUser();
+        return panel({
+            title: 'User Preferences',
+            className: model.background,
+            icon: Icon.options(),
+            item: hframe({
+                className: 'user-profile',
+                items: [
+                    vframe({
+                        className: 'user-profile--card',
+                        items: [
+                            h3(`${user.displayName}`),
+                            model.icon
+                        ]
+                    }),
+                    vframe({
+                        className: model.className,
+                        items: [
+                            p(`First Name: ${user.firstName}`),
+                            p(`Last Name: ${user.lastName}`),
+                            p(`Username: ${user.username}`),
+                            p(`Email: ${user.email}`)
+                        ]
+                    })
+                ]
+            })
+        });
+    }
+);
+
+const preferenceEditor = hoistCmp.factory(
+    () => {
+        return panel({
+            flex: 1,
+            height: 240,
+            title: 'Preference Editor',
+            icon: Icon.gears(),
+            compactHeader: true,
+            items: [
+                hbox({
+                    width: 'fill',
+                    items: [
+                        p('Set Text Color:'),
+                        select({
+                            bind: 'color',
+                            options: [
+                                {label: 'Red', value: 'red'},
+                                {label: 'Blue', value: 'blue'},
+                                {label: 'Green', value: 'green'}
+                            ]
+                        })
+                    ]
+                }),
+                hbox(
+                    p('Set User Icon:'),
+                    select({
+                        bind: 'userIcon',
+                        options: [
+                            {label: 'User', value: 'user'},
+                            {label: 'Chess Knight', value: 'knight'},
+                            {label: 'Sun', value: 'sun'}
+                        ]
+                    })
+                ),
+                hbox(
+                    switchInput({
+                        bind: 'showBackground',
+                        label: 'Show Background Image'
+                    })
+                )
+            ]
+        });
+    }
+);
+
+const codeOutput = hoistCmp.factory(
+    () => {
+        return panel({
+            compactHeader: true,
+            height: 240,
+            title: 'Output',
+            icon: Icon.code(),
+            model: {side: 'right', defaultSize: 300},
+            item: codeInput({
+                height: 230,
+                bind: 'codeOutput',
+                mode: 'javascript'
+            })
+        });
+    }
+);
+
 @HoistModel
 class Model {
-    @bindable color = XH.getPref('textColor');
-    @bindable userIcon = XH.getPref('userIcon');
-    @bindable showBackground = XH.getPref('showBackground');
-    @bindable key = '';
-    @bindable value = '';
-    @bindable codeOutput = '';
+    @bindable color = XH.getPref('prefExampleColor');
+    @bindable userIcon = XH.getPref('prefExampleIcon');
+    @bindable showBackground = XH.getPref('prefExampleShowBackground');
+    @bindable codeOutput = '// XH.setPref()';
 
     constructor() {
         this.addReaction({
             track: () => this.color,
-            run: () => this.setTextColor()
+            run: () => this.setPref('prefExampleColor', this.color)
         });
         this.addReaction({
             track: () => this.userIcon,
-            run: () => this.setIcon()
+            run: () => this.setPref('prefExampleIcon', this.userIcon)
         });
         this.addReaction({
             track: () => this.showBackground,
-            run: () => this.setBackground()
+            run: () => this.setPref('prefExampleShowBackground', this.showBackground)
         });
     }
 
-    setTextColor() {
-        this.setColor(this.color);
-        this.setKey('color');
-        this.setValue(this.color);
-        this.setCodeOutput(this.codeOutput + '\n' + `XH.setPref(${this.key}, ${this.value})`);
+    appendCodeOutput(key, value) {
+        this.setCodeOutput(this.codeOutput + '\n' + `XH.setPref('${key}', '${value}')`);
     }
 
-    setIcon() {
-        this.setUserIcon(this.userIcon);
-        this.setKey('userIcon');
-        this.setValue(this.userIcon);
-        this.setCodeOutput(this.codeOutput + '\n' + `XH.setPref(${this.key}, ${this.value})`);
-    }
-
-    setBackground() {
-        this.setKey('showBackground');
-        this.setValue(this.showBackground);
-        this.setCodeOutput(this.codeOutput + '\n' + `XH.setPref(${this.key}, ${this.value})`);
+    setPref(key, value) {
+        XH.setPref(key, value);
+        this.appendCodeOutput(key, value);
     }
 
     get className() {
-        const color = XH.getPref('textColor');
+        const {color} = this;
         switch (color) {
             case 'red':
                 return 'xh-red';
@@ -176,7 +186,7 @@ class Model {
     }
 
     get icon() {
-        const userIcon = XH.getPref('userIcon');
+        const {userIcon} = this;
         switch (userIcon) {
             case 'user':
                 return Icon.user({size: '5x'});
@@ -190,16 +200,6 @@ class Model {
     }
 
     get background() {
-        if (XH.getPref('showBackground')) {
-            return 'xh-tiled-bg';
-        } else {
-            return '';
-        }
-    }
-    saveAndReload() {
-        XH.setPref('textColor', this.color);
-        XH.setPref('userIcon', this.userIcon);
-        XH.setPref('showBackground', this.showBackground);
-        XH.reloadApp();
+        return this.showBackground ? 'xh-tiled-bg' : '';
     }
 }
