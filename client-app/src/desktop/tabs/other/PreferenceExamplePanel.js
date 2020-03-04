2dbe994d0fa29d9197d6588dad3bbc76d50e1f74
Petra Laohakul <petra@xh.io>
2020-03-04 12:42:14 -0500

    Major improvements to UI

--- client-app/src/desktop/tabs/other/PreferenceExamplePanel.js
+++ client-app/src/desktop/tabs/other/PreferenceExamplePanel.js
@@ -1,6 +1,6 @@
 import {wrapper} from '../../common';
 import {hoistCmp, HoistModel} from '@xh/hoist/core';
-import {box, code, hframe, p, vframe} from '@xh/hoist/cmp/layout';
+import {box, code, h3, hbox, hframe, p, vframe} from '@xh/hoist/cmp/layout';
 import {button} from '@xh/hoist/desktop/cmp/button';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {Icon} from '@xh/hoist/icon/Icon';
@@ -29,64 +29,85 @@ export const preferenceExamplePanel = hoistCmp.factory({
             items: [
                 panel({
                     title: 'User Preferences',
+                    className: model.background,
                     icon: Icon.options(),
-                    items: [
-                        vframe(
-                            panel({
+                    item: hframe({
+                        className: 'user-profile',
+                        items: [
+                            vframe({
+                                className: 'user-profile--card',
+                                items: [
+                                    h3(`${XH.getUser().displayName}`),
+                                    model.icon
+                                ]
+                            }),
+                            vframe({
                                 className: model.className,
-                                mask: model.mask,
-                                item: p('Change my text color!')
+                                items: [
+                                    p(`First Name: ${XH.getUser().firstName}`),
+                                    p(`Last Name: ${XH.getUser().lastName}`),
+                                    p(`Username: ${XH.getUser().username}`),
+                                    p(`Email: ${XH.getUser().email}`)
+                                ]
                             })
-                        )],
+                        ]
+                    }),
                     bbar: [
                         panel({
                             flex: 1,
-                            height: 250,
+                            height: 240,
                             title: 'PrefService at Work',
                             icon: Icon.gears(),
                             compactHeader: true,
                             items: [
-                                hframe(
-                                    p('Set Text Color:'),
-                                    select({
-                                        bind: 'color',
-                                        onChange: model.setTextColor(),
-                                        options: [
-                                            {label: 'red', value: 'red'},
-                                            {label: 'blue', value: 'blue'},
-                                            {label: 'green', value: 'green'}
-                                        ]
-                                    })
-                                ),
-                                hframe(
+                                hbox({
+                                    width: 'fill',
+                                    items: [p('Set Text Color:'),
+                                        select({
+                                            bind: 'color',
+                                            onChange: model.setTextColor(),
+                                            options: [
+                                                {label: 'Red', value: 'red'},
+                                                {label: 'Blue', value: 'blue'},
+                                                {label: 'Green', value: 'green'}
+                                            ]
+                                        })]
+                                }),
+                                hbox(
                                     p('Set User Icon:'),
                                     select({
                                         bind: 'userIcon',
+                                        onChange: model.setIcon(),
                                         options: [
-                                            {label: 'user', value: 'user'},
-                                            {label: 'blue', value: 'blue'},
-                                            {label: 'green', value: 'green'}
+                                            {label: 'User', value: 'user'},
+                                            {label: 'Chess Knight', value: 'knight'},
+                                            {label: 'Sun', value: 'sun'}
                                         ]
                                     })
                                 ),
-                                switchInput({
-                                    bind: 'mask',
-                                    label: 'Mask Component'
-                                }),
+                                hbox(
+                                    switchInput({
+                                        bind: 'showBackground',
+                                        label: 'Show Background Image',
+                                        onChange: () => model.setBackground()
+                                    })
+                                ),
                                 button({
-                                    text: 'Show something',
-                                    minimal: false
+                                    icon: Icon.refresh(),
+                                    text: 'Save Preferences and Reload',
+                                    minimal: false,
+                                    onClick: () => XH.reloadApp()
                                 })
                             ]
                         }),
                         panel({
                             compactHeader: true,
-                            height: 250,
+                            height: 240,
                             title: 'Output',
                             icon: Icon.code(),
                             model: {side: 'right', defaultSize: 300},
                             item: codeInput({
-                                height: 240
+                                height: 230
                             })
                         })
                     ]
@@ -99,13 +120,23 @@ export const preferenceExamplePanel = hoistCmp.factory({
 
 @HoistModel
 class Model {
-    @bindable mask = false
     @bindable color = XH.getPref('textColor');
-    @bindable userIcon;
+    @bindable userIcon = XH.getPref('userIcon');
+    @bindable showBackground = XH.getPref('showBackground');
 
     setTextColor() {
         this.setColor(this.color);
         XH.setPref('textColor', this.color);
+    }
+
+    setIcon() {
+        this.setUserIcon(this.userIcon);
+        XH.setPref('userIcon', this.userIcon);
+    }
+
+    setBackground() {
+        this.setShowBackground(!this.showBackground);
+        XH.setPref('showBackground', this.showBackground);
     }
 
     get className() {
@@ -119,6 +150,28 @@ class Model {
                 return 'xh-green';
             default:
                 return '';
+        }
+    }
+
+    get icon() {
+        const userIcon = XH.getPref('userIcon');
+        switch (userIcon) {
+            case 'user':
+                return Icon.user({size: '5x'});
+            case 'knight':
+                return Icon.chessKnight({size: '5x'});
+            case 'sun':
+                return Icon.sun(({size: '5x'}));
+            default:
+                return 'Please select an icon.';
+        }
+    }
+
+    get background() {
+        if (XH.getPref('showBackground')) {
+            return 'xh-tiled-bg';
+        } else {
+            return '';
         }
     }
 }
