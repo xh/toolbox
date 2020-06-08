7338e19e739d8ca643f3435bc84503a48899e37c
Anselm McClain <atm@xh.io>
2020-06-08 14:54:24 -0700

    Updated ag-grid license key

--- client-app/webpack.config.js
+++ client-app/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = (env = {}) => {
         appCode: 'toolbox',
         appName: 'Toolbox',
         appVersion: env.appVersion || '2.0-SNAPSHOT',
-        agGridLicenseKey: 'Extreme_Heavy_Industries__Scout_3Devs3_April_2020__MTU4NTg2ODQwMDAwMA==8f836aa25989abb0db36231c347dd436',
+        agGridLicenseKey: 'CompanyName=Extremely Heavy Industries,LicensedApplication=Toolbox,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=6,LicensedProductionInstancesCount=1,AssetReference=AG-008400,ExpiryDate=4_June_2021_[v2]_MTYyMjc2MTIwMDAwMA==e5556393a62b122c439b49dfe11f26d9',
         favicon: './public/favicon.png',
         devServerOpenPage: 'app/',
         ...env
