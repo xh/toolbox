c29e3c37e22b8616f88d6ba697b72bf69ad7aa06
Tom Tirapani <hello@tomtirapani.com>
2020-06-15 18:19:44 +0100

    Don't import AgGridUtils (#413)

--- client-app/src/Bootstrap.js
+++ client-app/src/Bootstrap.js
@@ -7,7 +7,6 @@
 import {installAgGrid} from '@xh/hoist/kit/ag-grid';
 import {installHighcharts} from '@xh/hoist/kit/highcharts';
 
-
 //-----------------------------------------------------------------
 // ag-Grid -- Import and Register
 // If you are using enterprise version, you must provide a license
@@ -18,10 +17,9 @@ import {installHighcharts} from '@xh/hoist/kit/highcharts';
 // import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham-dark.css';
 // import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css';
 // import {AgGridReact} from '@ag-grid-community/react';
-// import {_ as Utils} from '@ag-grid-community/core';
 // import {version} from '@ag-grid-community/all-modules/package.json';
 // ModuleRegistry.registerModules(AllCommunityModules);
-// installAgGridImpls(AgGridReact, Utils, version);
+// installAgGridImpls(AgGridReact, version);
 
 // Enterprise edition (w/license for Toolbox).
 import {AllModules, LicenseManager, ModuleRegistry} from '@ag-grid-enterprise/all-modules';
@@ -29,13 +27,12 @@ import '@ag-grid-enterprise/all-modules/dist/styles/ag-grid.css';
 import '@ag-grid-enterprise/all-modules/dist/styles/ag-theme-balham-dark.css';
 import '@ag-grid-enterprise/all-modules/dist/styles/ag-theme-balham.css';
 import {AgGridReact} from '@ag-grid-community/react';
-import {_ as Utils} from '@ag-grid-community/core';
 import {version} from '@ag-grid-enterprise/all-modules/package.json';
 ModuleRegistry.registerModules(AllModules);
 LicenseManager.setLicenseKey(
     'CompanyName=Extremely Heavy Industries,LicensedApplication=Toolbox,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=6,LicensedProductionInstancesCount=1,AssetReference=AG-008400,ExpiryDate=4_June_2021_[v2]_MTYyMjc2MTIwMDAwMA==e5556393a62b122c439b49dfe11f26d9'
 );
-installAgGrid(AgGridReact, Utils, version);
+installAgGrid(AgGridReact, version);
 
 
 //-------------------------------------------------------------------------------
