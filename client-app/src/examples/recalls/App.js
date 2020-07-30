b6a1a56fa6416d2f000a7a509f5d24019748b78f
lbwexler <lbwexler@xh.io>
2020-07-30 14:20:19 -0400

    Remove extraneous context menus

--- client-app/src/examples/recalls/App.js
+++ client-app/src/examples/recalls/App.js
@@ -1,6 +1,5 @@
 import {hoistCmp, uses} from '@xh/hoist/core';
 import {appBar} from '@xh/hoist/desktop/cmp/appbar';
-import {ContextMenuItem as CM} from '@xh/hoist/desktop/cmp/contextmenu';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {Icon} from '@xh/hoist/icon';
 import {AppModel} from './AppModel';
@@ -13,7 +12,6 @@ export const App = hoistCmp({
 
     render() {
         return panel({
-            contextMenu: [CM.reloadApp(), CM.about(), CM.logout()],
             tbar: appBar({
                 icon: Icon.health({size: '2x', prefix: 'fal'})
             }),
