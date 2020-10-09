41ba13c56a91f05992420575a11e95e5a7bc2a10
lbwexler <lbwexler@xh.io>
2020-10-08 20:14:27 -0400

    Revert "Dynamic Tabs Example"
    This reverts commit b01f0691

--- client-app/src/desktop/tabs/containers/TabPanelContainerPanel.js
+++ client-app/src/desktop/tabs/containers/TabPanelContainerPanel.js
@@ -1,12 +1,11 @@
-import {div, hspacer, hbox} from '@xh/hoist/cmp/layout';
-import {fmtTime} from '@xh/hoist/format';
+import {div, hspacer, span} from '@xh/hoist/cmp/layout';
 import {tabContainer, TabContainerModel} from '@xh/hoist/cmp/tab';
 import {creates, hoistCmp, HoistModel, managed} from '@xh/hoist/core';
 import {button} from '@xh/hoist/desktop/cmp/button';
 import {switchInput, textInput} from '@xh/hoist/desktop/cmp/input';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {Icon} from '@xh/hoist/icon';
-import {find, shuffle, isEmpty} from 'lodash';
+import {find} from 'lodash';
 import React from 'react';
 import {wrapper} from '../../common/Wrapper';
 
@@ -14,7 +13,7 @@ export const tabPanelContainerPanel = hoistCmp.factory({
     model: creates(() => new Model()),
 
     render({model}) {
-        const {stateTabModel, detachedTabModel, dynamicModel} = model;
+        const {stateTabModel, detachedTabModel, noTabsModel} = model;
 
         return wrapper({
             description: [
@@ -120,18 +119,15 @@ export const tabPanelContainerPanel = hoistCmp.factory({
                                 }
                             },
                             {
-                                id: 'dynamic',
-                                title: 'Dynamic',
+                                id: 'empty',
+                                title: 'Empty (no tabs)',
                                 content: () => {
                                     return panel({
                                         className: 'child-tabcontainer',
                                         bbar: [
-                                            button({icon: Icon.add(), text:  'Add', onClick: () => model.addDynamic()}),
-                                            button({icon: Icon.transaction(), text:  'Shuffle', onClick: () => model.shuffleDynamic()}),
-                                            button({icon: Icon.x(), text:  'Remove First', onClick: () => model.removeDynamic()}),
-                                            button({icon: Icon.xCircle(), text:  'Clear', onClick: () => model.clearDynamic()})
+                                            span('In this example, all tabs have been omitted and the default emptyText is shown.')
                                         ],
-                                        item: tabContainer({model: dynamicModel})
+                                        item: tabContainer({model: noTabsModel})
                                     });
                                 }
                             }
@@ -147,8 +143,6 @@ export const tabPanelContainerPanel = hoistCmp.factory({
 @HoistModel
 class Model {
 
-    id = 0;
-
     @managed
     detachedTabModel = new TabContainerModel(this.createContainerModelConfig({switcherPosition: 'none'}));
 
@@ -156,13 +150,12 @@ class Model {
     stateTabModel = new TabContainerModel(this.createContainerModelConfig({}));
 
     @managed
-    dynamicModel = new TabContainerModel({
-        tabs: []
+    noTabsModel = new TabContainerModel({
+        tabs: [
+            {id: 'omitted1', content: () => 'Not rendered!', omit: true},
+            {id: 'omitted2', content: () => 'Not rendered!', omit: true}
+        ]
     });
-
-    constructor() {
-        this.addDynamic();
-    }
 
     createContainerModelConfig(args) {
         const tabTxt = title => div(`This is the ${title} tab`);
@@ -186,39 +179,5 @@ class Model {
             ],
             ...args
         };
-    }
-
-    addDynamic() {
-        const {dynamicModel} = this,
-            id = this.id++;
-        dynamicModel.addTab({
-            id,
-            title: hbox(
-                `Tab ${id}`,
-                button({
-                    omit: true,
-                    icon: Icon.x(),
-                    onClick: () => dynamicModel.removeTab(id),
-                    style: {minHeight: 15, minWidth: 15, borderRadius: 5, padding: 0}
-                })
-            ),
-            content: () => div(`Tab ${id}: Brand spanking new at ${fmtTime(new Date(), {fmt: 'HH:mm:ss'})}`)
-        }, {activateImmediately: true});
-    }
-
-    removeDynamic() {
-        const {dynamicModel} = this;
-        if (!isEmpty(dynamicModel.tabs)) {
-            dynamicModel.removeTab(dynamicModel.tabs[0]);
-        }
-    }
-
-    clearDynamic() {
-        this.dynamicModel.setTabs([]);
-    }
-
-    shuffleDynamic() {
-        const {dynamicModel} = this;
-        dynamicModel.setTabs(shuffle(dynamicModel.tabs));
     }
 }
