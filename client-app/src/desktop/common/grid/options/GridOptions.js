4e64c8d87630fafb1a7596fa68c44488b5d454ac
Don <donedesign1@gmail.com>
2020-10-16 18:43:18 -0400

    Add option to demo Tree Styles (#429)

--- client-app/src/desktop/common/grid/options/GridOptions.js
+++ client-app/src/desktop/common/grid/options/GridOptions.js
@@ -1,8 +1,9 @@
 import {GridModel} from '@xh/hoist/cmp/grid';
-import {fragment, label, vspacer} from '@xh/hoist/cmp/layout';
+import {fragment, label, spacer, vspacer} from '@xh/hoist/cmp/layout';
 import {hoistCmp, uses} from '@xh/hoist/core';
+import {TreeStyle} from '@xh/hoist/core/enums/TreeStyle';
 import {button} from '@xh/hoist/desktop/cmp/button';
-import {textInput} from '@xh/hoist/desktop/cmp/input';
+import {select, textInput} from '@xh/hoist/desktop/cmp/input';
 import {Icon} from '@xh/hoist/icon';
 import {sample} from 'lodash';
 import {agGridOptions} from './AgGridOptions';
@@ -14,9 +15,26 @@ export const gridOptions = hoistCmp.factory({
         return fragment({
             items: [
                 agGridOptions({model: model.agGridModel}),
+                spacer({height: 10, omit: !model.treeMode}),
+                label({
+                    item: 'Tree Styles',
+                    omit: !model.treeMode
+                }),
+                select({
+                    bind: 'treeStyle',
+                    width: null,
+                    omit: !model.treeMode,
+                    options: [
+                        {value: TreeStyle.NONE, label: 'None'},
+                        {value: TreeStyle.HIGHLIGHT_GROUPS, label: 'Highlight Groups'},
+                        {value: TreeStyle.GROUP_BORDERS, label: 'Group Borders'},
+                        {value: TreeStyle.HIGHLIGHT_GROUPS_WITH_BORDERS, label: 'Highlight Groups w/Borders'}
+                    ]
+                }),
                 vspacer(10),
                 label('Empty text (quick-filter to test)'),
                 textInput({bind: 'emptyText', width: null}),
+                vspacer(10),
                 button({
                     text: 'Select random record',
                     icon: Icon.random(),
