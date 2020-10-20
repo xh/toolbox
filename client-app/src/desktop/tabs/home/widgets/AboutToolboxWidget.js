f0db459742b454d82dd2c5696ee11fa230879473
Anselm McClain <atm@xh.io>
2020-10-20 16:51:46 -0700

    Add Toolbar w/Feedback button on About widget

--- client-app/src/desktop/tabs/home/widgets/AboutToolboxWidget.js
+++ client-app/src/desktop/tabs/home/widgets/AboutToolboxWidget.js
@@ -1,6 +1,8 @@
-import {div, h2, span, table, tbody, td, th, tr} from '@xh/hoist/cmp/layout';
+import {div, filler, h2, span, table, tbody, td, th, tr} from '@xh/hoist/cmp/layout';
 import {relativeTimestamp} from '@xh/hoist/cmp/relativetimestamp';
 import {hoistCmp, XH} from '@xh/hoist/core';
+import {button} from '@xh/hoist/desktop/cmp/button';
+import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {fmtDateTime} from '@xh/hoist/format';
 import {Icon} from '@xh/hoist/icon';
 import {fmtTimeZone} from '@xh/hoist/utils/impl';
@@ -24,39 +26,53 @@ export const aboutToolboxWidget = hoistCmp.factory({
             hrVersion += ` (${fmtDateTime(snapDate)})`;
         }
 
-        return div({
+        return panel({
             className: 'tb-about-widget',
-            items: [
-                h2(Icon.info(), 'Deployment'),
-                table({
-                    item: tbody(
-                        row('App Name / Code', `${get('appName')} / ${get('appCode')}`),
-                        row('Version', `${get('appVersion')} (build ${get('appBuild')})`),
-                        row('Environment', get('appEnvironment')),
-                        row('App TZ', fmtTimeZone(get('appTimeZone'), get('appTimeZoneOffset'))),
-                        row('Server TZ', fmtTimeZone(get('serverTimeZone'), get('serverTimeZoneOffset'))),
-                        row('Client TZ', fmtTimeZone(get('clientTimeZone'), get('clientTimeZoneOffset'))),
-                        startupTime ? row('Server Uptime', relativeTimestamp({timestamp: startupTime, options: {pastSuffix: ''}})) : null
-                    )
+            item: div({
+                className: 'tb-about-widget__inner',
+                items: [
+                    h2(Icon.info(), 'Deployment'),
+                    table({
+                        item: tbody(
+                            row('App Name / Code', `${get('appName')} / ${get('appCode')}`),
+                            row('Version', `${get('appVersion')} (build ${get('appBuild')})`),
+                            row('Environment', get('appEnvironment')),
+                            row('App TZ', fmtTimeZone(get('appTimeZone'), get('appTimeZoneOffset'))),
+                            row('Server TZ', fmtTimeZone(get('serverTimeZone'), get('serverTimeZoneOffset'))),
+                            row('Client TZ', fmtTimeZone(get('clientTimeZone'), get('clientTimeZoneOffset'))),
+                            startupTime ? row('Server Uptime', relativeTimestamp({timestamp: startupTime, options: {pastSuffix: ''}})) : null
+                        )
+                    }),
+                    h2(Icon.server(), 'Server-Side Library Versions'),
+                    table({
+                        item: tbody(
+                            row('Hoist Core', get('hoistCoreVersion')),
+                            row('Grails', get('grailsVersion')),
+                            row('Java', get('javaVersion'))
+                        )
+                    }),
+                    h2(Icon.window(), 'Client-Side Library Versions'),
+                    table({
+                        item: tbody(
+                            row('Hoist React', hrVersion),
+                            row('React', get('reactVersion')),
+                            row('ag-Grid', get('agGridVersion')),
+                            row('Blueprint Core', get('blueprintCoreVersion')),
+                            row('MobX', get('mobxVersion'))
+                        )
+                    })
+                ]
+            }),
+            bbar: [
+                filler(),
+                button({
+                    text: 'Send Feedback!',
+                    icon: Icon.comment({className: 'fa-flip-horizontal'}),
+                    outlined: true,
+                    intent: 'primary',
+                    onClick: () => XH.showFeedbackDialog()
                 }),
-                h2(Icon.server(), 'Server-Side Library Versions'),
-                table({
-                    item: tbody(
-                        row('Hoist Core', get('hoistCoreVersion')),
-                        row('Grails', get('grailsVersion')),
-                        row('Java', get('javaVersion'))
-                    )
-                }),
-                h2(Icon.window(), 'Client-Side Library Versions'),
-                table({
-                    item: tbody(
-                        row('Hoist React', hrVersion),
-                        row('React', get('reactVersion')),
-                        row('ag-Grid', get('agGridVersion')),
-                        row('Blueprint Core', get('blueprintCoreVersion')),
-                        row('MobX', get('mobxVersion'))
-                    )
-                })
+                filler()
             ]
         });
     }
