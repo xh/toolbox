80b15d3ef57aedd651a6716611085b6b8d0c91f8
lbwexler <lbwexler@xh.io>
2020-10-28 16:40:27 -0400

    Restore snapshot versions for develop

--- client-app/webpack.config.js
+++ client-app/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = (env = {}) => {
     return configureWebpack({
         appCode: 'toolbox',
         appName: 'Toolbox',
-        appVersion: env.appVersion || '2.0-SNAPSHOT',
+        appVersion: env.appVersion || '3.0-SNAPSHOT',
         favicon: './public/favicon.png',
         devServerOpenPage: 'app/',
         dupePackageCheckExcludes: ['es-abstract'],
