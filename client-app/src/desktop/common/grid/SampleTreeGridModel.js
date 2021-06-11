13586cfde3172e28d148692ec8a192175c87ff29
lbwexler <lbwexler@xh.io>
2021-06-11 17:58:47 -0400

    Remove orphaned console logging

--- client-app/src/desktop/common/grid/SampleTreeGridModel.js
+++ client-app/src/desktop/common/grid/SampleTreeGridModel.js
@@ -65,7 +65,6 @@ export class SampleTreeGridModel extends HoistModel {
             dims = groupingChooserModel.value;
 
         const data = await XH.portfolioService.getPositionsAsync(dims, true);
-        console.log('tree data is ', data);
         if (isRefresh) {
             gridModel.updateData({update: data});
             if (isAutoRefresh) {
