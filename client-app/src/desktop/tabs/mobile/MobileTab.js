b8ef101ca8b375cdc03df2593ccd6125eb09cf80
Saba Schuttloffel <saba@xh.io>
2021-06-23 07:24:02 -0700

    Rearranged buttons

--- client-app/src/desktop/tabs/mobile/MobileTab.js
+++ client-app/src/desktop/tabs/mobile/MobileTab.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {hoistCmp, XH} from '@xh/hoist/core';
-import {a, hbox, hframe, img, p} from '@xh/hoist/cmp/layout';
+import {a, hbox, hframe, vframe, img, iframe, p} from '@xh/hoist/cmp/layout';
 import {button} from '@xh/hoist/desktop/cmp/button';
 import {Icon} from '@xh/hoist/icon';
 import {library} from '@fortawesome/fontawesome-svg-core';
@@ -18,9 +18,10 @@ export const mobileTab = hoistCmp.factory(
     () => wrapper({
         description: [
             <p>
-                Mobile apps share many of the same components and logic as their desktop counterparts, with a few differences of
-                using mobile-specific Hoist components, supported by the open source <a href={'http://onsen.io'}>Onsen</a> framework
-                to improve the native mobile UI experience.
+                Mobile apps share many of the same components and logic as their desktop counterparts, with some
+                mobile-specific Hoist components that are supported by the open source <a href={'http://onsen.io'}>Onsen
+                </a> framework to improve the native mobile UI experience. Explore the Toolbox Mobile App on your device
+                at <a href={'https://toolbox.xh.io/mobile'}>toolbox.xh.io/mobile</a>.
             </p>
         ],
         item: hbox({
@@ -29,40 +30,37 @@ export const mobileTab = hoistCmp.factory(
                 title: 'Mobile Support',
                 icon: Icon.icon({iconName: 'phone-laptop'}),
                 width: 1200,
-                height: 700,
+                height: 740,
                 items: [
-                    hframe(
+                    vframe(
                         button({
                             minimal: false,
                             text: 'Send Link to Toolbox Mobile App',
                             icon: Icon.icon({iconName: 'envelope'}),
-                            width: 'fit-content',
                             onClick: () => XH.fetchJson({url: 'emailMobileLink/send'})
                         }),
                         button({
                             minimal: false,
                             text: 'Launch Toolbox Mobile App',
                             icon: Icon.icon({iconName: 'mobile'}),
-                            width: 'fit-content',
-                            tooltip: 'Use Chrome Dev Tools to simulate a mobile device.',
                             onClick: () => window.open('/mobile')
                         })
                     ),
                     hframe(
-                        <iframe
-                            src='https://toolbox.xh.io/mobile'
-                            title='iframe-mobile'
-                            height='600'
-                        />
-                        // img({
-                        //     src: mobileImageHome
-                        // }),
-                        // img({
-                        //     src: mobileImageGrid
-                        // }),
-                        // img({
-                        //     src: mobileImageColChooser
+                        // iframe({
+                        //     src: 'https://toolbox.xh.io/mobile',
+                        //     title: 'iframe-mobile',
+                        //     height: '600'
                         // })
+                        img({
+                            src: mobileImageHome
+                        }),
+                        img({
+                            src: mobileImageGrid
+                        }),
+                        img({
+                            src: mobileImageColChooser
+                        })
                     )
                 ]
             })
