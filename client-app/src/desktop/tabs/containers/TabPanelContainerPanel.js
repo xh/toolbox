016c74eb67f4b564ca0f1c437cd09c54376e0256
Saba Schuttloffel <saba@xh.io>
2021-07-06 15:31:31 -0700

    Refactor to set title of tab

--- client-app/src/desktop/tabs/containers/TabPanelContainerPanel.js
+++ client-app/src/desktop/tabs/containers/TabPanelContainerPanel.js
@@ -1,4 +1,4 @@
-import {div, hbox, hspacer, vspacer, label, hframe} from '@xh/hoist/cmp/layout';
+import {div, hbox, hspacer, label} from '@xh/hoist/cmp/layout';
 import {fmtTime} from '@xh/hoist/format';
 import {tabContainer, TabContainerModel} from '@xh/hoist/cmp/tab';
 import {creates, managed, hoistCmp, HoistModel} from '@xh/hoist/core';
@@ -15,9 +15,7 @@ import {wrapper} from '../../common';
 export const tabPanelContainerPanel = hoistCmp.factory({
     model: creates(() => new Model()),
 
-    render({model}) {
-        const {stateTabModel, detachedTabModel, dynamicModel} = model;
-
+    render() {
         return wrapper({
             description: [
                 <p>
@@ -44,170 +42,7 @@ export const tabPanelContainerPanel = hoistCmp.factory({
                 width: 760,
                 height: 400,
                 item: tabContainer({
-                    model: {
-                        persistWith: {localStorageKey: 'tabExampleState'},
-                        tabs: [
-                            {
-                                id: 'top',
-                                title: 'Top Tabs',
-                                content: () => div(
-                                    `This overall example is a standard TabContainer with its switcher located in the default,
-                                    top position. Change the tabs above to see examples of other TabContainer configurations.`
-                                )
-                            },
-                            {
-                                id: 'bottom',
-                                title: 'Bottom Tabs',
-                                content: () => tabContainer({
-                                    className: 'child-tabcontainer',
-                                    model: model.createContainerModelConfig({
-                                        switcher: {orientation: 'bottom'}
-                                    })
-                                })
-                            },
-                            {
-                                id: 'left',
-                                title: 'Left Tabs',
-                                content: () => tabContainer({
-                                    className: 'child-tabcontainer',
-                                    model: model.createContainerModelConfig({
-                                        switcher: {orientation: 'left'}
-                                    })
-                                })
-                            },
-                            {
-                                id: 'right',
-                                title: 'Right Tabs',
-                                content: () => tabContainer({
-                                    className: 'child-tabcontainer',
-                                    model: model.createContainerModelConfig({
-                                        switcher: {orientation: 'right'}
-                                    })
-                                })
-                            },
-                            {
-                                id: 'custom',
-                                title: 'Custom Switcher',
-                                content: () => panel({
-                                    className: 'child-tabcontainer',
-                                    tbar: model.detachedTabModel.tabs.map(childModel => button({
-                                        intent: childModel.isActive ? 'primary' : null,
-                                        text: childModel.title,
-                                        onClick: () => {
-                                            detachedTabModel.setActiveTabId(childModel.id);
-                                        }
-                                    })),
-                                    item: tabContainer({model: detachedTabModel})
-                                })
-                            },
-                            {
-                                id: 'state',
-                                title: 'Tab State',
-                                content: () => {
-                                    const {tabs} = stateTabModel,
-                                        peopleTab = find(tabs, {id: 'people'}),
-                                        placesTab = find(tabs, {id: 'places'});
 
-                                    return panel({
-                                        className: 'child-tabcontainer',
-                                        bbar: [
-                                            switchInput({
-                                                model: peopleTab,
-                                                bind: 'disabled',
-                                                label: 'People Disabled?'
-                                            }),
-                                            hspacer(10),
-                                            'Places Tab Title: ',
-                                            textInput({
-                                                model: placesTab,
-                                                bind: 'title'
-                                            })
-                                        ],
-                                        item: tabContainer({model: stateTabModel})
-                                    });
-                                }
-                            },
-                            {
-                                id: 'dynamic',
-                                title: 'Dynamic',
-                                content: () => {
-                                    return panel({
-                                        className: 'child-tabcontainer',
-                                        bbar: [
-                                            button({icon: Icon.add(), text:  'Add', onClick: () => model.addDynamic()}),
-                                            button({icon: Icon.transaction(), text:  'Shuffle', onClick: () => model.shuffleDynamic()}),
-                                            button({icon: Icon.x(), text:  'Remove First', onClick: () => model.removeDynamic()}),
-                                            button({icon: Icon.xCircle(), text:  'Clear', onClick: () => model.clearDynamic()})
-                                        ],
-                                        item: tabContainer({model: dynamicModel})
-                                    });
-                                }
-                            },
-                            {
-                                id: 'badge',
-                                title: hbox({
-                                    items: [
-                                        'Tab with Badge',
-                                        badge({
-                                            tab: model.tabBadgeStyling,
-                                            item: model.badgeCount,
-                                            intent: model.intent !== 'none' ? model.intent: '',
-                                            omit: model.omitBadge ? !model.badgeCount : ''
-                                        })
-                                    ]
-                                }),
-                                content: () => {
-                                    return panel({
-                                        className: 'child-tabcontainer',
-                                        item: hframe(
-                                            div({
-                                                style: {padding: 10},
-                                                item: `Tabs can include a small, styled badge inline with the title, 
-                                                typically showing a count or other indicator that something is new or 
-                                                has content. When turned on, tab styling places the badge in the upper 
-                                                right-hand corner.`
-                                            }),
-                                            panel({
-                                                title: 'Display Options',
-                                                icon: Icon.settings(),
-                                                className: 'tbox-display-opts',
-                                                compactHeader: true,
-                                                model: {side: 'right', defaultSize: 250, resizable: false},
-                                                item: div({
-                                                    className: 'tbox-display-opts__inner',
-                                                    items: [
-                                                        label('Badge Intent'),
-                                                        select({
-                                                            bind: 'intent',
-                                                            options: ['primary', 'success', 'warning', 'danger', 'none']
-                                                        }),
-                                                        vspacer(10),
-                                                        label('Badge Value'),
-                                                        select({
-                                                            bind: 'badgeCount',
-                                                            options: [0, 1, 5, 10, 100]
-                                                        }),
-                                                        vspacer(10),
-                                                        switchInput({
-                                                            bind: 'omitBadge',
-                                                            label: 'Omit Badge if 0',
-                                                            labelSide: 'left'
-                                                        }),
-                                                        vspacer(10),
-                                                        switchInput({
-                                                            bind: 'tabBadgeStyling',
-                                                            label: 'Tab Badge Styling',
-                                                            labelSide: 'left'
-                                                        })
-                                                    ]
-                                                })
-                                            })
-                                        )
-                                    });
-                                }
-                            }
-                        ]
-                    }
                 })
             })
         });
@@ -217,6 +52,145 @@ export const tabPanelContainerPanel = hoistCmp.factory({
 class Model extends HoistModel {
 
     id = 0;
+
+    @bindable
+    badgeValue = 10;
+
+    @bindable
+    badgeIntent = 'none';
+
+    @managed
+    tabModel = new TabContainerModel({
+        persistWith: {localStorageKey: 'tabExampleState'},
+        tabs: [
+            {
+                id: 'top',
+                title: 'Top Tabs',
+                content: () => div(
+                    `This overall example is a standard TabContainer with its switcher located in the default,
+                                top position. Change the tabs above to see examples of other TabContainer configurations.`
+                )
+            },
+            {
+                id: 'bottom',
+                title: 'Bottom Tabs',
+                content: () => tabContainer({
+                    className: 'child-tabcontainer',
+                    model: this.createContainerModelConfig({
+                        switcher: {orientation: 'bottom'}
+                    })
+                })
+            },
+            {
+                id: 'left',
+                title: 'Left Tabs',
+                content: () => tabContainer({
+                    className: 'child-tabcontainer',
+                    model: this.createContainerModelConfig({
+                        switcher: {orientation: 'left'}
+                    })
+                })
+            },
+            {
+                id: 'right',
+                title: 'Right Tabs',
+                content: () => tabContainer({
+                    className: 'child-tabcontainer',
+                    model: this.createContainerModelConfig({
+                        switcher: {orientation: 'right'}
+                    })
+                })
+            },
+            {
+                id: 'custom',
+                title: 'Custom Switcher',
+                content: () => panel({
+                    className: 'child-tabcontainer',
+                    tbar: this.detachedTabModel.tabs.map(childModel => button({
+                        intent: childModel.isActive ? 'primary' : null,
+                        text: childModel.title,
+                        onClick: () => {
+                            this.detachedTabModel.setActiveTabId(childModel.id);
+                        }
+                    })),
+                    item: tabContainer({model: this.detachedTabModel})
+                })
+            },
+            {
+                id: 'state',
+                title: 'Tab State',
+                content: () => {
+                    const {tabs} = this.stateTabModel,
+                        peopleTab = find(tabs, {id: 'people'}),
+                        placesTab = find(tabs, {id: 'places'});
+
+                    return panel({
+                        className: 'child-tabcontainer',
+                        bbar: [
+                            switchInput({
+                                model: peopleTab,
+                                bind: 'disabled',
+                                label: 'People Disabled?'
+                            }),
+                            hspacer(10),
+                            'Places Tab Title: ',
+                            textInput({
+                                model: placesTab,
+                                bind: 'title'
+                            })
+                        ],
+                        item: tabContainer({model: this.stateTabModel})
+                    });
+                }
+            },
+            {
+                id: 'dynamic',
+                title: 'Dynamic',
+                content: () => {
+                    return panel({
+                        className: 'child-tabcontainer',
+                        bbar: [
+                            button({icon: Icon.add(), text:  'Add', onClick: () => this.addDynamic()}),
+                            button({icon: Icon.transaction(), text:  'Shuffle', onClick: () => this.shuffleDynamic()}),
+                            button({icon: Icon.x(), text:  'Remove First', onClick: () => this.removeDynamic()}),
+                            button({icon: Icon.xCircle(), text:  'Clear', onClick: () => this.clearDynamic()})
+                        ],
+                        item: tabContainer({model: this.dynamicModel})
+                    });
+                }
+            },
+            {
+                id: 'badge',
+                title: 'Badge',
+                content: () => {
+                    return panel({
+                        className: 'child-tabcontainer',
+                        items: [
+                            div({
+                                style: {padding: 10},
+                                item: `Tabs can include a small, styled badge inline with the title, typically showing a 
+                                count or other indicator that something is new or has content.`
+                            })
+                        ],
+                        bbar: [
+                            label('Badge Intent'),
+                            select({
+                                bind: 'badgeIntent',
+                                options: ['none', 'primary', 'success', 'warning', 'danger'],
+                                width: 100
+                            }),
+                            label('Badge Value'),
+                            select({
+                                bind: 'badgeValue',
+                                options: ['New', 'Overdue!', 1, 100],
+                                width: 100
+                            })
+                        ]
+                    });
+                }
+            }
+        ]
+    });
 
     @managed
     detachedTabModel = new TabContainerModel(this.createContainerModelConfig({switcher: false}));
@@ -233,18 +207,6 @@ class Model extends HoistModel {
         }
     });
 
-    @bindable
-    badgeCount = 10
-
-    @bindable
-    tabBadgeStyling = true
-
-    @bindable
-    omitBadge = false
-
-    @bindable
-    intent = 'primary'
-
     constructor() {
         super();
         makeObservable(this);
@@ -252,12 +214,26 @@ class Model extends HoistModel {
         this.addDynamic();
 
         this.addReaction({
-            track: () => this.intent,
-            run: (intent) => {
-                console.log(intent);
-            },
+            track: () => [this.badgeIntent, this.badgeValue],
+            run: () => this.updateBadge(),
             fireImmediately: true
         });
+    }
+
+    updateBadge() {
+        const tabWithBadge = this.tabModel.findTab('badge');
+        tabWithBadge.setTitle(
+            hbox({
+                items: [
+                    'Tab with Badge',
+                    badge({
+                        position: 'top',
+                        item: this.badgeValue,
+                        intent: this.badgeIntent
+                    })
+                ]
+            })
+        );
     }
 
     createContainerModelConfig(args) {
