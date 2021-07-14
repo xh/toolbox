a71be926a7365c89abc5fa3cfae4d25d5673f8c2
Saba Schuttloffel <saba@xh.io>
2021-07-14 11:56:40 -0700

    Render collapse icon if any row is expanded; align icon with rows

--- client-app/src/desktop/common/grid/SampleTreeGridModel.js
+++ client-app/src/desktop/common/grid/SampleTreeGridModel.js
@@ -121,7 +121,7 @@ export class SampleTreeGridModel extends HoistModel {
                     width: 200,
                     field: 'name',
                     isTreeColumn: true,
-                    expandFromHeader: true,
+                    headerHasExpandCollapse: true,
                     ...(includeCheckboxes ? this.createCheckboxTreeColumn() : {})
                 },
                 {
