acc8c4f0a6c6fc2f907505c0c0b543e556288976
Tom Tirapani <hello@tomtirapani.com>
2021-08-19 11:57:29 +0100

    Add library of reusable AppOptions for XH provided preferences

--- client-app/src/examples/portfolio/AppModel.js
+++ client-app/src/examples/portfolio/AppModel.js
@@ -1,8 +1,6 @@
 import {HoistAppModel, XH} from '@xh/hoist/core';
-import {button} from '@xh/hoist/desktop/cmp/button';
-import {buttonGroupInput} from '@xh/hoist/desktop/cmp/input';
+import {themeAppOption, sizingModeAppOption} from '@xh/hoist/desktop/cmp/appOption';
 import {Icon} from '@xh/hoist/icon';
-import {startCase} from 'lodash';
 import {OauthService} from '../../core/svc/OauthService';
 import {PortfolioService} from '../../core/svc/PortfolioService';
 
@@ -32,20 +30,8 @@ export class AppModel extends HoistAppModel {
 
     getAppOptions() {
         return [
-            {
-                name: 'sizingMode',
-                formField: {
-                    label: 'Default grid size',
-                    item: buttonGroupInput(
-                        getGridSizeModeButton('large'),
-                        getGridSizeModeButton('standard'),
-                        getGridSizeModeButton('compact'),
-                        getGridSizeModeButton('tiny')
-                    )
-                },
-                valueGetter: () => XH.sizingMode,
-                valueSetter: (v) => XH.setSizingMode(v)
-            }
+            themeAppOption,
+            sizingModeAppOption
         ];
     }
 
@@ -69,14 +55,4 @@ export class AppModel extends HoistAppModel {
             XH.hideBanner(category);
         }
     }
-}
-
-function getGridSizeModeButton(size) {
-    return button({
-        value: size,
-        text: startCase(size),
-        style: {
-            fontSize: `var(--xh-grid-${size}-font-size-px`
-        }
-    });
 }
