77b63c4836c38f496996317f49d656201c182795
Lee Wexler <lbwexler@xh.io>
2022-01-20 19:05:31 -0500

    App Package, remove extra variables (#550)

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -2,10 +2,4 @@ import io.xh.hoist.configuration.ApplicationConfig
 
 ApplicationConfig.defaultConfig(this)
 
-grails {
-    project.groupId = 'io.xh.toolbox'
-    app.context = '/'
-    resources.pattern = '/**'
-}
-
 hoist.enableWebSockets = true
