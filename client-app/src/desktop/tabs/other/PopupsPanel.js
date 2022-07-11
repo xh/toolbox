8f9b1edbf9fb11f0f5011a59b926074a2c2262a8
Tom Tirapani <hello@tomtirapani.com>
2022-07-11 10:31:33 +0100

    Linting

--- client-app/src/desktop/tabs/other/PopupsPanel.js
+++ client-app/src/desktop/tabs/other/PopupsPanel.js
@@ -119,7 +119,7 @@ export const popupsPanel = hoistCmp.factory(
                             onClick: () => XH.confirm({
                                 title: 'Confirm with promise',
                                 message: p('Confirm returns a promise that resolves to true if the user confirms or false if the user cancels.')
-                            }).then(responseToast),
+                            }).then(responseToast)
                         })
                     ),
                     row(
