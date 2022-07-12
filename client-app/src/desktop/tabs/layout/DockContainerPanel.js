f69c39a3aed56be94abee27dcf43f60055131267
Tom Tirapani <hello@tomtirapani.com>
2022-07-12 09:02:05 +0100

    Linting

--- client-app/src/desktop/tabs/layout/DockContainerPanel.js
+++ client-app/src/desktop/tabs/layout/DockContainerPanel.js
@@ -4,8 +4,8 @@ import {box, br, hbox} from '@xh/hoist/cmp/layout';
 import {Icon} from '@xh/hoist/icon';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {button} from '@xh/hoist/desktop/cmp/button';
-import {sampleGrid, wrapper} from '../../common';
 import {DockContainerModel, dockContainer} from '@xh/hoist/desktop/cmp/dock';
+import {sampleGrid, wrapper} from '../../common';
 
 export const dockContainerPanel = hoistCmp.factory({
     model: creates(() => DockContainerPanelModel),
