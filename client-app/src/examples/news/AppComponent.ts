45c26fe3c527047f127a2050ade1aced14842436
petedarinzo <peter.darinzo@gmail.com>
2022-10-20 16:24:47 -0400

    Revert hoistComponent to hoistCmp

--- client-app/src/examples/news/AppComponent.ts
+++ client-app/src/examples/news/AppComponent.ts
@@ -1,6 +1,6 @@
 import {a} from '@xh/hoist/cmp/layout';
 import {relativeTimestamp} from '@xh/hoist/cmp/relativetimestamp';
-import {hoistComponent, uses} from '@xh/hoist/core';
+import {hoistCmp, uses} from '@xh/hoist/core';
 import {appBar, appBarSeparator} from '@xh/hoist/desktop/cmp/appbar';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {Icon} from '@xh/hoist/icon';
@@ -8,7 +8,7 @@ import {AppModel} from './AppModel';
 import {newsPanel} from './NewsPanel';
 import '../../core/Toolbox.scss';
 
-export const AppComponent = hoistComponent({
+export const AppComponent = hoistCmp({
     displayName: 'App',
     model: uses(AppModel),
 
