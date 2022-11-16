2fa2e48b43d7c1a1b1c07c3bd7c69c767c8e8cd1
Colin Rudd <cnrudd@gmail.com>
2022-11-16 17:59:35 -0500

    event.argumentsArray solution implemented.

--- src/main/groovy/io/xh/toolbox/log/GrahamConverter.groovy
+++ src/main/groovy/io/xh/toolbox/log/GrahamConverter.groovy
@@ -1,12 +1,72 @@
 package io.xh.toolbox.log
 
 import ch.qos.logback.classic.pattern.ClassicConverter;
-import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.ILoggingEvent
+
+import static io.xh.hoist.util.Utils.exceptionRenderer
+import static io.xh.hoist.util.Utils.identityService
 
 class GrahamConverter extends ClassicConverter {
 
-          @Override
-          public String convert(ILoggingEvent event) {
-                  return event.message.replaceAll(/ \| /, ', ')
+      @Override
+      public String convert(ILoggingEvent event) {
+          def msg = event.message
+          def args = event.argumentArray
+          def username = identityService?.username
+
+          List<String> ret = args.collect { arg ->
+              switch (arg) {
+                  case List: return delimitedTxt(arg.flatten())
+                  default: return delimitedTxt([arg])
               }
+          }.flatten()
+
+          if (msg) ret << addMsgKey(quoteSentence(msg))
+          if (username) ret.add(ret.size() - 1, "user=$username")
+
+          return ret.join(', ')
       }
+
+
+    //---------------------------------------------------------------------------
+    // Implementation
+    //---------------------------------------------------------------------------
+    private List<String> delimitedTxt(List msgs) {
+        List<String> ret = msgs.collect {
+            it instanceof Throwable ? exceptionRenderer.summaryTextForThrowable(it) :
+                    it instanceof Map ? kvTxt(it) :
+                            addMsgKey(it.toString())
+        }
+
+        def (messages, meta) = ret.flatten().split {it.startsWith('message=')}
+        messages = messages.collect {it.replace('message=', '')}.join(' | ')
+        messages = messages ? "message=${quoteSentence(messages)}" : ''
+        meta << messages
+        return meta
+    }
+
+
+    private List kvTxt(Map msgs) {
+        List<String> ret = msgs.collect {k,v ->
+            v = v instanceof Throwable ?
+                    exceptionRenderer.summaryTextForThrowable(v) :
+                    quoteSentence(v.toString())
+            return "$k=$v"
+        }
+        return ret
+    }
+
+    private String quoteSentence(String str) {
+        if (str.contains(' ') && !str.startsWith('"')) {
+            str = '"' + str + '"'
+        }
+        return str
+    }
+
+    private String addMsgKey(String str) {
+        if (str.split('=').size() != 2) {
+            str = "message=$str"
+        }
+        return str
+    }
+}
