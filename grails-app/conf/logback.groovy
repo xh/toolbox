851206af523f3e1e2d51f49610782764443d664c
Colin Rudd <cnrudd@gmail.com>
2022-12-02 13:07:51 -0500

    Fix monitorLayout override.

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,8 +1,8 @@
 import io.xh.hoist.configuration.LogbackConfig
 import io.xh.toolbox.log.TokenizedExampleConverter
 
-// Tokenized output to monitorLayout
-LogbackConfig.monitorLayout = '%d{HH:mm:ss.SSS}, %tokenizedMsg%n'
+// Tokenized output to monitorLayout, overriding msg conversion from DefaultConverter
+LogbackConfig.monitorLayout = '%d{HH:mm:ss.SSS} | %tokenizedMsg%n'
 
 conversionRule("tokenizedMsg", TokenizedExampleConverter)
 
