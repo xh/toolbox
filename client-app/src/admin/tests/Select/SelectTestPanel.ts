74514ba3b6479d71a578d5e42597a94da824a6db
Colin Rudd <cnrudd@gmail.com>
2023-05-16 17:48:38 -0400

    demo for: https://github.com/xh/hoist-react/issues/3366 - reduces width of select and has enableTooltipsOnTags: true to demo tooltips on tags.

--- client-app/src/admin/tests/Select/SelectTestPanel.ts
+++ client-app/src/admin/tests/Select/SelectTestPanel.ts
@@ -97,13 +97,14 @@ export const SelectTestPanel = hoistCmp({
                         }
                     }),
                     example({
-                        name: 'Select with leftIcon & enableMulti & enableClear & rsOptions: {hideSelectedOptions: false, closeMenuOnSelect: false}',
+                        name: 'Select with leftIcon & enableMulti & enableTooltipsOnTags & enableClear & rsOptions: {hideSelectedOptions: false, closeMenuOnSelect: false}',
                         bind: 'enableMultiMenuOpen',
                         selectProps: {
-                            width: 350,
+                            width: 200,
                             options: usStates,
                             leftIcon: Icon.globe(),
                             enableMulti: true,
+                            enableTooltipsOnTags: true,
                             placeholder: 'Select state(s)...',
                             enableClear: true,
                             hideSelectedOptions: false,
