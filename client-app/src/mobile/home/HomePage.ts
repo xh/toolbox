c29d125d1e2e90ec3d0c158d96d013223fe737b4
Tom Tirapani <hello@tomtirapani.com>
2023-11-02 20:40:39 +0000

    Add ZoneGrid example usages to desktop and mobile (#679)

--- client-app/src/mobile/home/HomePage.ts
+++ client-app/src/mobile/home/HomePage.ts
@@ -25,6 +25,13 @@ export const homePage = hoistCmp.factory({
                     route: 'treegrids'
                 }),
                 summaryCard({
+                    title: 'Zone Grids',
+                    icon: Icon.gridLarge(),
+                    summary:
+                        'Zone Grids leverage an underlying Grid to render multi-line full-width rows.',
+                    route: 'zoneGrid'
+                }),
+                summaryCard({
                     title: 'Dataview',
                     icon: Icon.addressCard(),
                     summary:
