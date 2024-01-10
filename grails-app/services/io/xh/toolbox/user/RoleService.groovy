702d0a079584eab4378726ff7bd5dfc10ae20ed3
Greg Solomon <88904581+ghsolomon@users.noreply.github.com>
2024-01-10 17:39:15 -0500

    Assign local admin roles if needed (#691)
    * Move local admin role assignment from Bootstrap to RoleService.init
    
    * Fix GORM annotation for RoleService.assignLocalAdminRolesIfNeeded

--- grails-app/services/io/xh/toolbox/user/RoleService.groovy
+++ grails-app/services/io/xh/toolbox/user/RoleService.groovy
@@ -1,5 +1,23 @@
 package io.xh.toolbox.user
 
+import grails.gorm.transactions.Transactional
 import io.xh.hoist.role.provided.DefaultRoleService
 
-class RoleService extends DefaultRoleService {}
+import static io.xh.hoist.util.InstanceConfigUtils.getInstanceConfig
+
+class RoleService extends DefaultRoleService {
+    @Override
+    void init() {
+        super.init()
+        assignLocalAdminRolesIfNeeded()
+    }
+
+    @Transactional
+    private void assignLocalAdminRolesIfNeeded() {
+        String adminUsername = getInstanceConfig('adminUsername')
+        if (adminUsername) {
+            def user = User.findByEmail(adminUsername)
+            roleAdminService.ensureUserHasRoles(user, ['HOIST_ADMIN', 'HOIST_ADMIN_READER', 'HOIST_ROLE_MANAGER'])
+        }
+    }
+}
