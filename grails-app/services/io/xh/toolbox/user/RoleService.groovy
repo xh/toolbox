577b0d5a96d3b3c771ff4f287d0fd5cecbcb5426
Anselm McClain <atm@xh.io>
2024-01-11 13:37:02 -0800

    Minor comment updates

--- grails-app/services/io/xh/toolbox/user/RoleService.groovy
+++ grails-app/services/io/xh/toolbox/user/RoleService.groovy
@@ -5,6 +5,12 @@ import io.xh.hoist.role.provided.DefaultRoleService
 
 import static io.xh.hoist.util.InstanceConfigUtils.getInstanceConfig
 
+/**
+ * Toolbox leverages Hoist's built-in, database-backed Role management and its associated Admin Console UI.
+ *
+ * @see io.xh.hoist.role.provided.DefaultRoleService for details on this out-of-the-box option for Roles.
+ * @see io.xh.hoist.role.BaseRoleService for details on how to implement an alternate, entirely custom approach.
+ */
 class RoleService extends DefaultRoleService {
     @Override
     void init() {
