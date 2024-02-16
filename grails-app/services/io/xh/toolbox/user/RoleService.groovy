8cc3a2e4095c81ce62b974351b77d397113be986
Anselm McClain <atm@xh.io>
2024-02-16 06:43:28 -0800

    Update to latest DefaultRoleService.ensureUserHasRoles() API

--- grails-app/services/io/xh/toolbox/user/RoleService.groovy
+++ grails-app/services/io/xh/toolbox/user/RoleService.groovy
@@ -23,7 +23,7 @@ class RoleService extends DefaultRoleService {
         String adminUsername = getInstanceConfig('adminUsername')
         if (adminUsername) {
             def user = User.findByEmail(adminUsername)
-            defaultRoleEditService.ensureUserHasRoles(user, 'HOIST_ADMIN')
+            ensureUserHasRoles(user, 'HOIST_ADMIN')
         }
     }
 }
