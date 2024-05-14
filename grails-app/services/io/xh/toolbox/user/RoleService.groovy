1137d3b1f4f8a730542894c4ff4230720d1e3f1b
Lee Wexler <lbwexler@xh.io>
2024-05-14 15:40:47 -0400

    Config to support mock directory group lookups (#717)
    + Support new `mockDirectoryGroups` config to simulate dir group lookups and test Hoist admin console + role service functionality
    
    ---------
    
    Co-authored-by: Anselm McClain <atm@xh.io>

--- grails-app/services/io/xh/toolbox/user/RoleService.groovy
+++ grails-app/services/io/xh/toolbox/user/RoleService.groovy
@@ -1,9 +1,6 @@
 package io.xh.toolbox.user
 
-import grails.gorm.transactions.Transactional
 import io.xh.hoist.role.provided.DefaultRoleService
-
-import static io.xh.hoist.util.InstanceConfigUtils.getInstanceConfig
 
 /**
  * Toolbox leverages Hoist's built-in, database-backed Role management and its associated Admin Console UI.
@@ -13,8 +10,25 @@ import static io.xh.hoist.util.InstanceConfigUtils.getInstanceConfig
  */
 class RoleService extends DefaultRoleService {
 
-    /** Toolbox does not use any external directory (such as LDAP/AD) for group membership. */
-    boolean getDirectoryGroupsSupported() {
-        return false
+    /**
+     * Toolbox does not currently connect to an external directory, but supports a `mockDirectoryGroups` config
+     * so we can simulate directory group lookups and see all group-related controls in the Admin Console Roles UI.
+     *
+     * Config should be JSON formatted like: `{"testGroupName": ["user1@example.com", "user2@example.com"]}`.
+     *
+     * This mock code also supports use of the special group name `sim_error` to mock a lookup failure.
+     */
+    protected Map<String, Object> doLoadUsersForDirectoryGroups(Set<String> groups, boolean strictMode) {
+        def config = configService.getMap('mockDirectoryGroups', [:])
+
+        return groups.collectEntries { group ->
+            if (config[group]) return [group, config[group] as Set]
+            if (group == 'sim_error') {
+                def e = new RuntimeException('There was a simulated error looking up directory groups.')
+                if (strictMode) throw e
+                logError('There was an error', e)
+            }
+            return [group, [] as Set]
+        }
     }
 }
