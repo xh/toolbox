91f923edc4b70dfe3b82bc3d69179bc3433d8cff
Anselm McClain <atm@xh.io>
2024-05-14 12:30:02 -0700

    Tweak mocked directory group lookup handling
    + Config name -> `mockDirectoryGroups`
    + Remove need for `xh:` prefix

--- grails-app/services/io/xh/toolbox/user/RoleService.groovy
+++ grails-app/services/io/xh/toolbox/user/RoleService.groovy
@@ -10,15 +10,20 @@ import io.xh.hoist.role.provided.DefaultRoleService
  */
 class RoleService extends DefaultRoleService {
 
+    /**
+     * Toolbox does not currently connect to an external directory, but supports a `mockDirectoryGroups` config
+     * so we can simulate directory group lookups and see all group-related controls in the Admin Console Roles UI.
+     *
+     * Config should be JSON formatted like: `{"testGroupName": ["user1@example.com", "user2@example.com"]}`.
+     *
+     * This mock code also supports use of the special group name `sim_error` to mock a lookup failure.
+     */
     protected Map<String, Object> doLoadUsersForDirectoryGroups(Set<String> groups, boolean strictMode) {
-        def config = configService.getMap('testDirectories', [:])
+        def config = configService.getMap('mockDirectoryGroups', [:])
 
         return groups.collectEntries { group ->
-            if (!group.startsWith('xh:')) return [group, "Directory name should start with 'xh:'"];
-
-            def key = group.takeAfter('xh:')
-            if (config[key]) return [group, config[key] as Set]
-            if (key == 'sim_error') {
+            if (config[group]) return [group, config[group] as Set]
+            if (group == 'sim_error') {
                 def e = new RuntimeException('There was a simulated error looking up directory groups.')
                 if (strictMode) throw e
                 logError('There was an error', e)
