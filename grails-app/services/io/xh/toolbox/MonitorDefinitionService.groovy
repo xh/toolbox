7be63ecc3fb6012dc4045ad8e4b2c58bec2eb7f2
Anselm McClain <atm@xh.io>
2024-05-15 06:59:07 -0700

    Remove broken status monitor, re-order/comment remaining

--- grails-app/services/io/xh/toolbox/MonitorDefinitionService.groovy
+++ grails-app/services/io/xh/toolbox/MonitorDefinitionService.groovy
@@ -25,34 +25,8 @@ class MonitorDefinitionService extends DefaultMonitorDefinitionService {
     PortfolioService portfolioService
     RecallsService recallsService
 
-    /**
-     * Check the count of news stories loaded by NewsService
-     */
-    def newsStoryCount(MonitorResult result) {
-        result.metric = newsService.itemCount
-    }
-
-    /**
-     * Always returns the value 1337 and a message
-     */
-    def metric1337Monitor(MonitorResult result) {
-        result.metric = 1337
-        result.message = 'This metric is always 1337!'
-    }
-
-    /**
-     * A monitor that attempts to divide by zero
-     */
-    def divideByZeroMonitor(MonitorResult result){
-        result.message = 'Trying to divide by zero'
-        result.metric = 1 / (1-1)
-    }
-
-    /**
-     * Check when the last update to the news was fetched.
-     * If no news stories have been fetched at all, we consider that a failure.
-     */
-    def lastUpdateAgeMins(MonitorResult result) {
+    /** Report when the latest news update was fetched, or fail if no stories are loaded. */
+    def newsLastUpdateMins(MonitorResult result) {
         if (newsService.lastTimestamp) {
             def diffMs = currentTimeMillis() - newsService.lastTimestamp.time,
                 diffMins = Math.floor(diffMs / MINUTES)
@@ -64,27 +38,26 @@ class MonitorDefinitionService extends DefaultMonitorDefinitionService {
         }
     }
 
-    /**
-     * Check whether or not the NewsService has loaded stories from all its sources.
-     */
-    def loadedSourcesCount(MonitorResult result) {
+    /** Count news stories loaded by NewsService. */
+    def newsStoryCount(MonitorResult result) {
+        result.metric = newsService.itemCount
+    }
+
+    /** Check if NewsService has loaded stories from all its sources. */
+    def newsLoadedSourcesCount(MonitorResult result) {
         result.metric = newsService.loadedSourcesCount
         result.status = newsService.allSourcesLoaded ? OK : FAIL
     }
 
-    /**
-     * Check the storage space used by uploaded files in the FileManager app, in megabytes
-     */
-    def storageSpaceUsed(MonitorResult result) {
+    /** Storage space used by uploaded files in the FileManager app, in megabytes. */
+    def fileManagerStorageUsedMb(MonitorResult result) {
         // sum up the sizes of all uploaded files as MB
         def bytes = fileManagerService.list()
                 .sum {it.length()} ?: 0
         result.metric = ((double)(bytes / (1024 * 1024))).round(2)
     }
 
-    /**
-     * Check whether or not we connected to the FDA server successfully for drug recall information.
-     */
+    /** Check ability to connect to the FDA API for drug recall example app. */
     def recallsFetchStatus(MonitorResult result) {
         recallsService.fetchRecalls('')
         def code = recallsService.lastResponseCode
@@ -98,30 +71,17 @@ class MonitorDefinitionService extends DefaultMonitorDefinitionService {
         }
     }
 
-    /**
-     * Check the current number of positions in the Portfolio example
-     */
-    def positionCount(MonitorResult result) {
+    /** Count of positions in the Portfolio example. */
+    def rawPositionCount(MonitorResult result) {
         result.metric = portfolioService.portfolio.rawPositions.size()
     }
 
-    /**
-     * Check the current number of instruments in the Portfolio example
-     */
+    /** Count of instruments in the Portfolio example. */
     def instrumentCount(MonitorResult result) {
         result.metric = portfolioService.portfolio.instruments.size()
     }
 
-    /**
-     * Check when the most recent prices in the Portfolio example were generated
-     */
-    def pricesAgeMs(MonitorResult result) {
-        result.metric = currentTimeMillis() - portfolioService.portfolio.timeCreated.time
-    }
-
-    /**
-     * Count the number of commits loaded from GitHub.
-     */
+    /** Count of commits loaded from GitHub. */
     def gitHubCommitCount(MonitorResult result) {
         def repos = configService.getList('gitHubRepos', []),
             commitCount = 0
@@ -140,9 +100,7 @@ class MonitorDefinitionService extends DefaultMonitorDefinitionService {
         result.message = "Loaded ${commitCount} commits from ${repos.size()} repos."
     }
 
-    /**
-     * Check the age of the most recent commit loaded from GitHub.
-     */
+    /** Age of the most recent commit loaded from GitHub, in minutes. */
     def gitHubMostRecentCommitAgeMins(MonitorResult result) {
         def repos = configService.getList('gitHubRepos', []),
             maxDate = null
@@ -168,4 +126,17 @@ class MonitorDefinitionService extends DefaultMonitorDefinitionService {
             result.message = "Commits not loaded, or could not determine latest commit."
         }
     }
+
+    /** Attempt to divide by zero, to demonstrate built-in exception handling. */
+    def divideByZeroMonitor(MonitorResult result){
+        result.message = 'Trying to divide by zero'
+        result.metric = 1 / (1-1)
+    }
+
+    /** Always returns the value 1337 and a message, to demonstrate the wonders of the number 1337. */
+    def metric1337Monitor(MonitorResult result) {
+        result.metric = 1337
+        result.message = 'This metric is always 1337!'
+    }
+
 }
