055484a2005efa9900d2979129741c9ed2271e5c
lbwexler <lbwexler@xh.io>
2024-07-01 17:04:25 -0400

    Checkpoint -- use new AuthModel lifecycle

--- client-app/src/BaseAppModel.ts
+++ client-app/src/BaseAppModel.ts
@@ -1,15 +1,6 @@
-import {HoistAppModel, XH} from '@xh/hoist/core';
-import {AuthService} from './core/svc/AuthService';
+import {HoistAppModel} from '@xh/hoist/core';
 
 export class BaseAppModel extends HoistAppModel {
-    static override async preAuthAsync() {
-        await XH.installServicesAsync(AuthService);
-    }
-
-    override async logoutAsync() {
-        return XH.authService.logoutAsync();
-    }
-
     override get supportsVersionBar(): boolean {
         return window.self === window.top;
     }
