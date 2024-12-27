66b9411d43b744cfcb5dc554a01aab1c457b5c15
Lee Wexler <lbwexler@xh.io>
2024-12-26 20:37:50 -0500

    RefreshButton takes target instead of model (#748)

--- client-app/src/examples/portfolio/grid/PositionsGrid.ts
+++ client-app/src/examples/portfolio/grid/PositionsGrid.ts
@@ -29,7 +29,7 @@ export const positionsGrid = hoistCmp.factory({
                 gridCountLabel({unit: 'position'}),
                 filler(),
                 relativeTimestamp({bind: 'loadTimestamp'}),
-                refreshButton({model: XH.refreshContextModel, intent: 'success'})
+                refreshButton({target: XH.refreshContextModel, intent: 'success'})
             ]
         });
     }
