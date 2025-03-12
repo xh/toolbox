f0987831b1890f3d424c9b792b0491d911ab9e01
David Ryan Hall <david.ryan.hall@gmail.com>
2025-03-12 09:56:26 -0700

    fix: entry point app name change contactMobile -> mobileContacts
     - webpack seemed to want to load the base "contacts" every time any path was requested with "contact" in the name
     - contactMobile was always being loaded using the contact.js bunble
     - mobile seems not to have the same issue, so mobileContacts is loading with no issue

--- client-app/src/apps/mobileContacts.ts
+++ client-app/src/apps/mobileContacts.ts
@@ -2,12 +2,12 @@ import '../Bootstrap';
 
 import {XH} from '@xh/hoist/core';
 import {AppContainer} from '@xh/hoist/mobile/appcontainer';
-import {AppComponent} from '../examples/contactMobile/AppComponent';
-import {AppModel} from '../examples/contactMobile/AppModel';
+import {AppComponent} from '../examples/mobileContacts/AppComponent';
+import {AppModel} from '../examples/mobileContacts/AppModel';
 import {AuthModel} from '../core/AuthModel';
 
 XH.renderApp({
-    clientAppCode: 'contactMobile',
+    clientAppCode: 'mobileContacts',
     clientAppName: 'XH Contact Mobile',
     componentClass: AppComponent,
     modelClass: AppModel,
