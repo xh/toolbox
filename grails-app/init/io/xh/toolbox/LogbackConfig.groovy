03d43ea94cfb316776630813beaac8fac146577d
lbwexler <lbwexler@xh.io>
2025-09-27 15:55:27 -0400

    Checkpoint

--- grails-app/init/io/xh/toolbox/LogbackConfig.groovy
+++ grails-app/init/io/xh/toolbox/LogbackConfig.groovy
@@ -2,14 +2,17 @@ package io.xh.toolbox
 
 import io.xh.toolbox.log.CustomLogSupportConverter
 
+/**
+ * Example of deeply overriding format via custom converter.
+ */
 class LogbackConfig extends io.xh.hoist.LogbackConfig {
 
+    String getMonitorLayout() {
+        '%d{HH:mm:ss.SSS} | %customMsg%n'
+    }
+
     void configureLogging() {
-
-        // Example deeply overriding format via custom converter.
-        LogbackConfig.monitorLayout = '%d{HH:mm:ss.SSS} | %customMsg%n'
         conversionRule('customMsg', CustomLogSupportConverter)
-
         super.configureLogging()
     }
 }
