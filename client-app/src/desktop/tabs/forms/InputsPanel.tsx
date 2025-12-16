cc53a97c7d9360bfa53b5d14636c67620bd5a2cf
Ryanseanlee <ryanseanlee@gmail.com>
2025-12-15 19:13:49 -0800

    Commit with snapshot

--- client-app/src/desktop/tabs/forms/InputsPanel.tsx
+++ client-app/src/desktop/tabs/forms/InputsPanel.tsx
@@ -127,7 +127,7 @@ const formContents = hoistCmp.factory<InputsPanelModel>(({model}) =>
                     row({
                         field: 'jsonInput',
                         layout: {height: 260},
-                        item: jsonInput()
+                        item: jsonInput({language: 'json'})
                     })
                 ]
             }),
