3c6e679ae3a1904ce5867a45db2903d44eb298b8
John Haynes <jm.haynes.42@gmail.com>
2026-04-23 14:53:41 -0400

    Checkpoint

--- client-app/src/desktop/tabs/grids/ColumnChooserPanel.ts
+++ client-app/src/desktop/tabs/grids/ColumnChooserPanel.ts
@@ -1,12 +1,13 @@
 import {grid, GridModel} from '@xh/hoist/cmp/grid';
-import {filler, hframe, p} from '@xh/hoist/cmp/layout';
+import {filler, hframe, p, span} from '@xh/hoist/cmp/layout';
 import {storeFilterField} from '@xh/hoist/cmp/store';
 import {creates, hoistCmp, HoistModel, managed, XH} from '@xh/hoist/core';
 import {colChooserButton, exportButton} from '@xh/hoist/desktop/cmp/button';
 import {columnChooser} from '@xh/hoist/desktop/cmp/grid';
+import {switchInput} from '@xh/hoist/desktop/cmp/input';
 import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {Icon} from '@xh/hoist/icon';
-import {makeObservable} from '@xh/hoist/mobx';
+import {bindable, makeObservable, observable} from '@xh/hoist/mobx';
 import {wrapper} from '../../common';
 import {
     actualGrossCol,
@@ -40,6 +41,11 @@ export const columnChooserPanel = hoistCmp.factory({
                         flex: 1,
                         item: grid({model: model.gridModel}),
                         tbar: [
+                            span('Lock Column Groups'),
+                            switchInput({
+                                model,
+                                bind: 'lockColumnGroups'
+                            }),
                             filler(),
                             storeFilterField({gridModel: model.gridModel}),
                             colChooserButton({gridModel: model.gridModel}),
@@ -58,12 +64,31 @@ export const columnChooserPanel = hoistCmp.factory({
 });
 
 class ColumnChooserPanelModel extends HoistModel {
-    @managed gridModel: GridModel;
+    @managed @observable.ref gridModel: GridModel;
+    @bindable lockColumnGroups: boolean = true;
 
     constructor() {
         super();
         makeObservable(this);
-        this.gridModel = new GridModel({
+        this.gridModel = this.createGridModel(this.lockColumnGroups);
+
+        this.addReaction({
+            track: () => this.lockColumnGroups,
+            run: lockColumnGroups => {
+                XH.safeDestroy(this.gridModel);
+                this.gridModel = this.createGridModel(lockColumnGroups);
+                this.loadAsync().catchDefault();
+            }
+        });
+    }
+
+    override async doLoadAsync(loadSpec) {
+        const sales = await XH.fetchJson({url: 'sales'});
+        this.gridModel.loadData(sales);
+    }
+
+    private createGridModel(lockColumnGroups: boolean): GridModel {
+        return new GridModel({
             store: {
                 idSpec: data => `${data.firstName}~${data.lastName}~${data.city}~${data.state}`
             },
@@ -71,18 +96,42 @@ class ColumnChooserPanelModel extends HoistModel {
             emptyText: 'No records found...',
             colChooserModel: true,
             enableExport: true,
+            lockColumnGroups,
             columns: [
                 {
                     groupId: 'demographics',
                     children: [
-                        {...fullNameCol},
-                        {...firstNameCol, hidden: true},
-                        {...lastNameCol, hidden: true},
-                        {...cityCol, hidden: true},
-                        {...stateCol}
+                        {
+                            ...fullNameCol,
+                            chooserDescription:
+                                'Concatenation of first and last name, rendered as a single cell.'
+                        },
+                        {
+                            ...firstNameCol,
+                            hidden: true,
+                            chooserDescription: 'Given name of the sales representative.'
+                        },
+                        {
+                            ...lastNameCol,
+                            hidden: true,
+                            chooserDescription: 'Family name of the sales representative.'
+                        },
+                        {
+                            ...cityCol,
+                            hidden: true,
+                            chooserDescription: 'City where the sales rep is based.'
+                        },
+                        {
+                            ...stateCol,
+                            chooserDescription:
+                                'Two-letter US state code where the sales rep is based.'
+                        }
                     ]
                 },
-                {...salaryCol},
+                {
+                    ...salaryCol,
+                    chooserDescription: 'Base annual salary in USD, excluding bonuses.'
+                },
                 {
                     groupId: 'sales',
                     headerName: 'Sales',
@@ -92,23 +141,44 @@ class ColumnChooserPanelModel extends HoistModel {
                             groupId: 'projected',
                             borders: false,
                             headerAlign: 'center',
-                            children: [{...projectedUnitsSoldCol}, {...projectedGrossCol}]
+                            children: [
+                                {
+                                    ...projectedUnitsSoldCol,
+                                    chooserDescription:
+                                        'Forecasted unit count for the current period, set at the start of the year.'
+                                },
+                                {
+                                    ...projectedGrossCol,
+                                    chooserDescription:
+                                        'Forecasted gross revenue (USD) based on projected units and list price.'
+                                }
+                            ]
                         },
                         {
                             groupId: 'actual',
                             borders: false,
                             headerAlign: 'center',
-                            children: [{...actualUnitsSoldCol}, {...actualGrossCol}]
+                            children: [
+                                {
+                                    ...actualUnitsSoldCol,
+                                    chooserDescription:
+                                        'Actual unit count sold to date for the current period.'
+                                },
+                                {
+                                    ...actualGrossCol,
+                                    chooserDescription:
+                                        'Actual gross revenue (USD) recognized to date for the current period.'
+                                }
+                            ]
                         }
                     ]
                 },
-                {...retainCol}
+                {
+                    ...retainCol,
+                    chooserDescription:
+                        'Whether the sales rep should be retained for the next fiscal year.'
+                }
             ]
         });
-    }
-
-    override async doLoadAsync(loadSpec) {
-        const sales = await XH.fetchJson({url: 'sales'});
-        this.gridModel.loadData(sales);
     }
 }
