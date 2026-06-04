698571521d1e55c5e221f0b19afa3e701afa0a16
Jakub Kupsik <jskupsik@gmail.com>
2026-06-04 09:41:46 -0400

    Removed old acceptedFileTypes

--- client-app/src/examples/filemanager/FileManager.ts
+++ client-app/src/examples/filemanager/FileManager.ts
@@ -5,7 +5,6 @@ import {panel} from '@xh/hoist/desktop/cmp/panel';
 import {toolbarSep} from '@xh/hoist/desktop/cmp/toolbar';
 import {button} from '@xh/hoist/desktop/cmp/button';
 import {fileChooser} from '@xh/hoist/desktop/cmp/filechooser';
-import {Accept} from '@xh/hoist/kit/react-dropzone';
 import {grid} from '@xh/hoist/cmp/grid';
 import {FileManagerModel} from './FileManagerModel';
 import './FileManager.scss';
@@ -66,19 +65,3 @@ export const fileManager = hoistCmp.factory({
         });
     }
 });
-
-// Entire example is limited to admins, but still limit to arbitrary-but-reasonable list of
-// accepted file types for sanity (and to demo the `accepts` prop).
-const acceptedFileTypes: Accept = {
-    'text/plain': ['.txt'],
-    'image/png': ['.png'],
-    'image/gif': ['.gif'],
-    'image/jpeg': ['.jpg'],
-    'application/msword': ['.doc'],
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': ['.docx'],
-    'application/vnd.ms-excel': ['.xls'],
-    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx'],
-    'application/vnd.ms-powerpoint': ['.ppt'],
-    'application/vnd.openxmlformats-officedocument.presentationml.presentation': ['.pptx'],
-    'application/pdf': ['.pdf']
-};
