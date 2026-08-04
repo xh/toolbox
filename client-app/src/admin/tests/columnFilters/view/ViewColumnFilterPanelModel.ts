440815838a4452d99ed315002d1c1b0502344d57
lbwexler <lbwexler@xh.io>
2026-08-04 12:32:40 -0400

    ViewColumnFilterPanel: set reuseRecords: 'cubeRowVersion' on View-connected store

--- client-app/src/admin/tests/columnFilters/view/ViewColumnFilterPanelModel.ts
+++ client-app/src/admin/tests/columnFilters/view/ViewColumnFilterPanelModel.ts
@@ -132,6 +132,7 @@ export class ViewColumnFilterPanelModel extends HoistModel {
             treeMode: true,
             store: {
                 projectionOnly: true,
+                reuseRecords: 'cubeRowVersion',
                 idEncodesTreePath: true
             },
             treeStyle: TreeStyle.HIGHLIGHTS_AND_BORDERS,
