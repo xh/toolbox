af7ac8dfc650955dccb0502f31dc18b5eb8e4c2f
lbwexler <lbwexler@xh.io>
2026-09-18 16:08:16 -0400

    Format import left by codemod

--- client-app/src/desktop/tabs/grids/ExternalSortGridPanelModel.ts
+++ client-app/src/desktop/tabs/grids/ExternalSortGridPanelModel.ts
@@ -1,5 +1,5 @@
 import {XH, HoistModel, managed, LoadSpec, PlainObject} from '@xh/hoist/core';
-import { bindable } from '@xh/hoist/mobx';
+import {bindable} from '@xh/hoist/mobx';
 import {GridModel, localDate, ExcelFormat} from '@xh/hoist/cmp/grid';
 import {fmtNumberTooltip, millionsRenderer, numberRenderer} from '@xh/hoist/format';
 
